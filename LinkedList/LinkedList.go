fac1b2d270576db212a8209f532bfbd79e3e4f75
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-09-24 03:03:06 +0100

    added LLRev, really just a copy not really needed may delete -AP

--- LinkedList/LinkedList.go
+++ LinkedList/LinkedList.go
@@ -72,6 +72,19 @@ func RevRecur(node *LinkedNode) *LinkedNode {
 
 }
 
+func LLRev(currNode *LinkedNode) *LinkedNode{ // 6,5,4,3,2,1 
+											  // 6,5,4,3,   1 > 2 > nil
+											  // 6,5,4,,   1 > 2 > nil
+
+	if currNode.Next == nil{
+		return currNode
+	}
+	returnedHead := LLRev(currNode.Next)
+	currNode.Next.Next = currNode
+	currNode.Next = nil
+	return returnedHead
+	
+}
 //***************Linked List******************
 type LinkedList struct {
 	Head *LinkedNode
