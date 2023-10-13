035928c3a6e2c834c1fd73f3e7c12ef592c7a2d1
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-10-14 03:22:06 +0900

    BuyHighSellLow, 75% better -AP

--- Patterns/SlidingWindow.go
+++ Patterns/SlidingWindow.go
@@ -55,3 +55,20 @@ func PermutationOfString(str1, str2 string) bool{
 
 	return false
 }
+
+func BuyHighSellLow(prices []int) int{
+	lowest := prices[0] //Originally did -999999, but that would mean that we could possibly get a worse conidition
+	maxSell := -9999999999
+
+	for _, v := range prices {
+		possibleMS := v - lowest
+		if lowest > v {
+			lowest = v
+		}
+
+		if possibleMS > maxSell{
+			maxSell = possibleMS
+		}
+	}
+	return maxSell
+}
