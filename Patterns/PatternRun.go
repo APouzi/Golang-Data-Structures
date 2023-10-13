40c624c325dc69ae0d3108bd730463aa030fe8a9
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-10-14 03:35:35 +0900

    we added RunPattern functionality to allow us to concentrate all of the pattern runs to the package, have it run in the main.go -AP

--- Patterns/PatternRun.go
+++ Patterns/PatternRun.go
@@ -1,5 +1,11 @@
 package patterns
 
-func runPattern() {
+import "fmt"
 
+func RunPattern() {
+	returnRes := PermutationOfString("dcda", "adc")
+	fmt.Println(returnRes)
+
+	returnRes2 := BuyHighSellLow([]int{6, -5, 4, 6, 1, 2, 5, 6, 5, 4, 6, 1, 2, 5, 6, 5, 4, 6, 1, 2, 11})
+	fmt.Println(returnRes2)
 }
