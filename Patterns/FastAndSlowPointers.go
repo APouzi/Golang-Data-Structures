1f71377ece7c4ca19e35fc1ac83ab2d5d29914fd
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-10-15 20:37:16 +0900

    we now add the cycle detect from LinkedList, it's just a lesson thing but it's here for keeps sake, will probably remove, idk yet -AP

--- Patterns/FastAndSlowPointers.go
+++ Patterns/FastAndSlowPointers.go
@@ -1,11 +1,13 @@
 package patterns
 
+import "fmt"
+
 func HappyNumber(n int) bool {
 	SquareSumDigit := func(num int) int {
 		currsum := 0
 		for num > 0 {
 			digit := num % 10
-			currsum += digit
+			currsum += digit ^ 2
 			num /= 10
 		}
 		return currsum
@@ -24,4 +26,25 @@ func HappyNumber(n int) bool {
 	}
 
 	return slow == 1
+}
+
+
+// This is just a placeholder, we are not actually running this because it's currently inside of the linkedlist
+type LinkedNode struct{
+	Next *LinkedNode
+	Val string
+}
+func CycleDetect(node *LinkedNode) bool {
+	fast := node
+	slow := node
+
+	for fast != nil || slow != nil { //Here we are stopping if the linked list is a not a cycle
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow { //If it is a cycle we are gauranteed to stop because eventually this will become true
+			fmt.Println("connected at: ", fast.Val)
+			return true
+		}
+	}
+	return false
 }
