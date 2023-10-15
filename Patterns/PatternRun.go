9aa566f6fb16da1050c41801e9f87f9158a20d82
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-10-15 18:52:25 +0900

    This is going to be run for the happy number slow and fast pattern -AP

--- Patterns/PatternRun.go
+++ Patterns/PatternRun.go
@@ -8,4 +8,8 @@ func RunPattern() {
 
 	returnRes2 := BuyHighSellLow([]int{6, -5, 4, 6, 1, 2, 5, 6, 5, 4, 6, 1, 2, 5, 6, 5, 4, 6, 1, 2, 11})
 	fmt.Println(returnRes2)
+
+	HappyInput := 19
+	HappyNum := HappyNumber(HappyInput)
+	fmt.Println(fmt.Sprintf("Is %d happy num? Answer %t", HappyInput,HappyNum))
 }
