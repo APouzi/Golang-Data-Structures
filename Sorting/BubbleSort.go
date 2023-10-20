a9186f18eaca5302cdeab300d86f2fc12972e1e6
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-10-21 06:53:23 +0900

    added BubbleSort Algorithims -AP

--- Sorting/BubbleSort.go
+++ Sorting/BubbleSort.go
@@ -1,1 +1,31 @@
 package sorting
+
+import "fmt"
+
+func BubbleSort(arr []int) {
+	steps := 0
+	for i := 0; i < len(arr); i ++{
+		for j := 1; j < len(arr)-i;j++{
+			steps++
+			if arr[j] < arr[j-1]{
+				arr[j], arr[j-1]= arr[j-1], arr[j]
+			}
+			
+		}
+	}
+	fmt.Println("steps bubblesort 1:",steps)
+}
+
+func BubbleSort2(arr []int) {
+	steps := 0
+	for i := 0; i < len(arr); i ++{
+		for j := 1; j < len(arr)-i-1;j++{
+			steps++
+			if arr[j] < arr[j+1]{
+				arr[j], arr[j+1]= arr[j+1], arr[j]
+			}
+			
+		}
+	}
+	fmt.Println("steps bubblesort :",steps)
+}
