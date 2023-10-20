a04168975819240a717a6e3d6a904020c68f548a
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-10-21 06:53:47 +0900

    added InsertionSort algorithim -AP

--- Sorting/SelectionSort.go
+++ Sorting/SelectionSort.go
@@ -1,1 +1,20 @@
 package sorting
+
+import "fmt"
+
+func InsertionSortAlgo(arr []int) {
+	min := 0
+	steps := 0
+
+	for i := 0; i < len(arr); i++ {
+		min = i //You must always move this back to the left most part of the shrinking window
+		for j := i; j < len(arr); j++ {
+			if arr[j] < arr[min] {
+				min = j
+			}
+			steps++
+		}
+		arr[i], arr[min] = arr[min], arr[i]
+	}
+	fmt.Println("steps for algo",steps)
+}
