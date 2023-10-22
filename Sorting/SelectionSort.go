1cbbb2d72e4db525003d081b850a08354c8aa73e
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-10-23 05:10:37 +0900

    Selection sort has note about how to get the min window -AP

--- Sorting/SelectionSort.go
+++ Sorting/SelectionSort.go
@@ -7,9 +7,9 @@ func SelectionSortAlgo(arr []int) {
 	steps := 0
 
 	for i := 0; i < len(arr); i++ {
-		min = i //You must always move this back to the left most part of the shrinking window
-		for j := i; j < len(arr); j++ {
-			if arr[j] < arr[min] {
+		min = i //You must always move this back to the left most part of the shrinking window.
+		for j := i; j < len(arr)-i-1; j++ {
+			if arr[j] < arr[min] { // This cannot be "arr[j]<arr[i]", as sometimes I want to do because of the fact that this needs to get the smallest, comparing to "i" won't help. Need to get the smallest. 
 				min = j
 			}
 			steps++
