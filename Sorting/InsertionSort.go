a45b9479a44884a5c43b2acd405f8707933c483f
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-10-22 23:39:54 +0900

    added the actual insertion sort, since selection sort was here and now moved -AP

--- Sorting/InsertionSort.go
+++ Sorting/InsertionSort.go
@@ -1,1 +1,16 @@
 package sorting
+
+func InsertionSortAlgo(arr []int) {
+	var sorted int
+	var key int
+	for i := 1; i < len(arr); i++ {
+		sorted = i - 1
+		key = arr[i]
+		for sorted >= 0 && key < arr[sorted] {
+			arr[sorted+1] = arr[sorted]
+			sorted--
+		}
+		arr[sorted+1] = key
+
+	}
+}
