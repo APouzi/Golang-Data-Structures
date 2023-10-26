2d123312a386f8821d807ad7f7aa6eca420a1b32
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-10-26 21:13:01 +0900

    Insert getting the array sizes in aLL and ALR, arrL and arrR. Which will for one, get the size of the indexes and then we will initate the size of arrays -AP

--- Sorting/MergeSort.go
+++ Sorting/MergeSort.go
@@ -11,3 +11,13 @@ func MergeSort(array []int, st int, end int) {
 		Merge(array, st, mid, end) //We then merge the left and right arrays. This process will repeat over and over because of the fact that the stack at the bottom won't until the very last steps. The bottom will have the two biggest size of the arrays.
 	}
 }
+
+func Merge(array []int, st int, mid int, end int) {
+	fmt.Println("inputs st, mid, end", st, mid, end)
+	aLL := mid - st + 1 //This is just getting the size of left array 
+	aLR := end - mid // This is just getting the size of left array  
+	fmt.Println(aLL,aLR)
+	arrL := make([]int, aLL) //Here we don't use []int{}, unless we want to do appending operation, which would be more costly
+	arrR := make([]int, aLR) //Here we don't use []int{}, unless we want to do appending operation, which would be more costly
+	
+}
