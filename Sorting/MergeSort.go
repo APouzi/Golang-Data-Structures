b8c805e3496eb8aa6106af5392d64dcb10f688ac
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-10-26 21:11:33 +0900

    I have added the recursive merge calls to split up the array, this only gets the indexs and size of arrays -AP

--- Sorting/MergeSort.go
+++ Sorting/MergeSort.go
@@ -1,1 +1,13 @@
 package sorting
+
+import "fmt"
+
+func MergeSort(array []int, st int, end int) {
+	if st < end {
+		mid := (st + end) / 2 
+		MergeSort(array, st, mid) //First break up the left side of the array, recursively and this will be put into the stack until we reach the base case of "st < end" being the size of 0, 1 is the smallest
+		MergeSort(array, mid+1, end) //Pop it out the stack and then break up the rest, to the right. 
+		// Note for the two top 
+		Merge(array, st, mid, end) //We then merge the left and right arrays. This process will repeat over and over because of the fact that the stack at the bottom won't until the very last steps. The bottom will have the two biggest size of the arrays.
+	}
+}
