85abfa211a73865cde58dfbb894cacb37f31ae0b
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-10-26 21:29:14 +0900

    runAlgo Merge sort will look alittle cleaner with comment saying what it is and clear spacing -AP

--- Sorting/RunSortAlgos.go
+++ Sorting/RunSortAlgos.go
@@ -11,4 +11,13 @@ func RunSorting() {
 	fmt.Println("BubbleSort",arr)
 	InsertionSortAlgo(InsertionSort)
 	fmt.Println("Insertion Sort",InsertionSort)
+
+	// ----------Merge Sort----------
+	fmt.Println("---Merge Sort Algorithim has begun----")
+	// MergeSortNum := []int{9,8,7,6,5,4,3,2,1,11,12,34,64,7654,123,1236,124,1235,643,67,123,565,3123}
+	MergeSortNum := []int{6,5,12,10,9,1}
+	MergeSort(MergeSortNum, 0, len(MergeSortNum)-1)
+	fmt.Println("Merge Sort Algorithm",MergeSortNum)
+
+
 }
