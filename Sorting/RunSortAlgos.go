1210329ec06e5067a33daa6e7ab931b3d0c7c0d2
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-10-29 09:19:34 +0900

    Here we are going to be handling the pass off and print out of the Merging list -AP

--- Sorting/RunSortAlgos.go
+++ Sorting/RunSortAlgos.go
@@ -19,12 +19,41 @@ func RunSorting() {
 	InsertionSortAlgo(InsertionSort)
 	fmt.Println("Insertion Sort",InsertionSort)
 
-	// ----------Merge Sort----------
+	// ----------Merge Sort (Array and Linked List)----------
 	fmt.Println("---Merge Sort Algorithim has begun----")
 	// MergeSortNum := []int{9,8,7,6,5,4,3,2,1,11,12,34,64,7654,123,1236,124,1235,643,67,123,565,3123}
 	MergeSortNum := []int{6,5,12,10,9,1}
 	MergeSort(MergeSortNum, 0, len(MergeSortNum)-1)
 	fmt.Println("Merge Sort Algorithm",MergeSortNum)
 
+	MergeHead := &Node{val:0}
+	MergeHead.next = &Node{val:1}
+	MergeHead.next.next = &Node{val:5}
+	MergeHead.next.next.next = &Node{val:10}
+	MergeHead.next.next.next.next = &Node{val:-1}
 
+	head := &Node{val: 5}
+	head.next = &Node{val: 2}
+	head.next.next = &Node{val: 1}
+	head.next.next.next = &Node{val: 3}
+	head.next.next.next.next = &Node{val: 4}
+
+	fmt.Println("Original linked list:")
+	printLinkedList(head)
+
+	sortedHead := MergeSortLinkedList(head)
+
+	fmt.Println("Sorted linked list:")
+	printLinkedList(sortedHead)
 }
+
+func printLinkedList(head *Node) {
+	current := head
+	for current != nil {
+		fmt.Print(current.val, " -> ")
+		current = current.next
+	}
+	fmt.Println("nil")
+}
+
+
