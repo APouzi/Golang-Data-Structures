620407c5334d13b373e241102cd6d2d5dacea65d
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-10-29 09:18:46 +0900

    Added the Node for LinkedList pass off -AP

--- Sorting/RunSortAlgos.go
+++ Sorting/RunSortAlgos.go
@@ -2,6 +2,13 @@ package sorting
 
 import "fmt"
 
+type Node struct{
+	val int
+	next *Node
+}
+
+
+
 func RunSorting() {
 	arr := []int{5, 4, 3, 2, 1}
 	InsertionSort := []int{9,8,7,6,5,4,3,2,1,11,12,34,64,7654,123,1236,124,1235,643,67,123,565,3123}
