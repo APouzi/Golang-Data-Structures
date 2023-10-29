3920aaf3afe63c596674158b32bd489e8e7c7bbc
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-10-29 22:02:30 +0900

    fixed up the formatting of the runSortAlgos file for organization and formatting. -AP

--- Sorting/RunSortAlgos.go
+++ Sorting/RunSortAlgos.go
@@ -10,14 +10,15 @@ type Node struct{
 
 
 func RunSorting() {
-	arr := []int{5, 4, 3, 2, 1}
-	InsertionSort := []int{9,8,7,6,5,4,3,2,1,11,12,34,64,7654,123,1236,124,1235,643,67,123,565,3123}
-	// arr2 := []int{5, 4, 3, 2, 1}
-	BubbleSort(arr)
-	// BubbleSort2(arr2)
-	fmt.Println("BubbleSort",arr)
-	InsertionSortAlgo(InsertionSort)
-	fmt.Println("Insertion Sort",InsertionSort)
+	BubbleSortNums := []int{9,8,7,6,5,4,3,2,1,11,12,34,64,7654,123,1236,124,1235,643,67,123,565,3123}
+	InsertionSortNums := []int{9,8,7,6,5,4,3,2,1,11,12,34,64,7654,123,1236,124,1235,643,67,123,565,3123}
+	// ----BubbleSort----
+	BubbleSort(BubbleSortNums)
+	fmt.Println("BubbleSort",BubbleSortNums)
+	
+	// ----Selection Sort----
+	SelectionSortAlgo(InsertionSortNums)
+	fmt.Println("Insertion Sort",InsertionSortNums)
 
 	// ----------Merge Sort (Array and Linked List)----------
 	fmt.Println("---Merge Sort Algorithm has begun---")
