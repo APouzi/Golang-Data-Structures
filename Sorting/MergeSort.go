bc548186a2dbedad6ef58e850b2d6d48e383ef08
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-10-29 21:53:50 +0900

    added helpful notes for mergeSort -AP

--- Sorting/MergeSort.go
+++ Sorting/MergeSort.go
@@ -8,7 +8,7 @@ func MergeSort(array []int, st int, end int) {
 		MergeSort(array, st, mid)    //First break up the left side of the array, recursively and this will be put into the stack until we reach the base case of "st < end" being the size of 0, 1 is the smallest
 		MergeSort(array, mid+1, end) //Pop it out the stack and then break up the rest, to the right.
 		// Note for the two top
-		Merge(array, st, mid, end) //We then merge the left and right arrays. This process will repeat over and over because of the fact that the stack at the bottom won't until the very last steps. The bottom will have the two biggest size of the arrays.
+		Merge(array, st, mid, end) //We then merge the left and right arrays. This process will repeat over and over because of the fact that the stack at the bottom won't until the very last steps. The bottom will have the two biggest size of the arrays. Note: We are passing in the left and right array but getting the st, mid and end. As we peel of the stack we will have the st, mid and end because of the fact that the stack will hold the parameters passed in (st and end) and the variablie created(mid)
 	}
 }
 
