4ac64d8f6d5a20f4db8e71fd8841d5b809ed50fb
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-10-30 19:31:33 +0900

    added some helpful comments regarding quicksort -AP

--- Sorting/QuickSort.go
+++ Sorting/QuickSort.go
@@ -3,8 +3,9 @@ package sorting
 func QuickSort(arr []int, low, high int) {
 	if low < high {
 
-		pivot := Partition(arr, low, high)
+		pivot := Partition(arr, low, high) //The pivot, like mid in merge is going to be the divider, except that the pivot will be in the correct place when done so we need to use this as the divider and get the pivot (+-1) and recuservly do this until we are done.
 
+		// Unlike mergesort, the actual array isn't being divided and then merged together into a new array. This is just getting the source array and the indexs will be altering or "dividing" until we reach elements that are the size of one. Once they are that size, they are technically completed
 		QuickSort(arr, low, pivot-1)
 		QuickSort(arr, pivot+1, high)
 	}
