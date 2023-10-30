1ec47f14c4dcba0539638d6f1bebbcdd8582390f
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-10-30 19:20:22 +0900

    quicksort recursion start has been created -AP

--- Sorting/QuickSort.go
+++ Sorting/QuickSort.go
@@ -1,1 +1,12 @@
 package sorting
+
+func QuickSort(arr []int, low, high int) {
+	if low < high {
+
+		pivot := Partition(arr, low, high)
+
+		QuickSort(arr, low, pivot-1)
+		QuickSort(arr, pivot+1, high)
+	}
+}
+
