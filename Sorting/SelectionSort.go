2dc8ff95532bc5428c3b6870bc5eb0cb50755a69
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-10-31 09:24:49 +0900

    fixed SelectionSort -AP

--- Sorting/SelectionSort.go
+++ Sorting/SelectionSort.go
@@ -8,7 +8,7 @@ func SelectionSortAlgo(arr []int) {
 
 	for i := 0; i < len(arr); i++ {
 		min = i //You must always move this back to the left most part of the shrinking window.
-		for j := i; j < len(arr)-i-1; j++ {
+		for j := i; j < len(arr); j++ {
 			if arr[j] < arr[min] { // This cannot be "arr[j]<arr[i]", as sometimes I want to do because of the fact that this needs to get the smallest, comparing to "i" won't help because it will not be (always) the smallest, or biggest
 				min = j
 			}
