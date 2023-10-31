a66e6356667db3140ab4a12d591f0e36f9468f9d
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-10-31 09:03:59 +0900

    adjusting the print statements regarding the output, along with some changes -AP

--- Sorting/RunSortAlgos.go
+++ Sorting/RunSortAlgos.go
@@ -11,7 +11,7 @@ type Node struct{
 
 func RunSorting() {
 	BubbleSortNums := []int{9,8,7,6,5,4,3,2,1,11,12,34,64,7654,123,1236,124,1235,643,67,123,565,3123}
-	InsertionSortNums := []int{9,8,7,6,5,4,3,2,1,11,12,34,64,7654,123,1236,124,1235,643,67,123,565,3123}
+	
 	// ----BubbleSort----
 	BubbleSort(BubbleSortNums)
 	fmt.Println("BubbleSort",BubbleSortNums)
@@ -21,19 +21,18 @@ func RunSorting() {
 	fmt.Println("Insertion Sort",InsertionSortNums)
 
 	// ----------Merge Sort (Array and Linked List)----------
-	fmt.Println("---Merge Sort Algorithm has begun---")
+	fmt.Println("\n---Merge Sort Algorithm has begun---")
 	// MergeSortNum := []int{9,8,7,6,5,4,3,2,1,11,12,34,64,7654,123,1236,124,1235,643,67,123,565,3123}
 	MergeSortNum := []int{6,5,12,10,9,1}
 	MergeSort(MergeSortNum, 0, len(MergeSortNum)-1)
 	// sortedPrac := MergeSortPrac(MergeSortNum)
-	fmt.Println("Merge Sort Algorithm",MergeSortNum)
-
+	fmt.Println("MergeSort Algorithm Result",MergeSortNum)
 	fmt.Println("---Merge Sort Algorithm 2 has begun---")
 	MergeSortNum2 := []int{6,1,7,8,10}
 	SortedMergeSort2 := MergeSort2(MergeSortNum2)
-	fmt.Println("Merge Sort Algorithm", SortedMergeSort2)
+	fmt.Println("MergeSort2 Algorithm Result", SortedMergeSort2)
 
-	fmt.Println("--- Merge Sort Algorithm LinkedList has begun ---")
+	fmt.Println("\n--- Merge Sort Algorithm LinkedList has begun ---")
 	MergeHead := &Node{val:0}
 	MergeHead.next = &Node{val:1}
 	MergeHead.next.next = &Node{val:5}
@@ -54,7 +53,8 @@ func RunSorting() {
 	fmt.Println("Sorted linked list:")
 	printLinkedList(sortedHead)
 
-	fmt.Println("--- QuickSort Algorithim has begun ---")
+	// ----Quicksort Algorithm----
+	fmt.Println("\n--- QuickSort Algorithim has begun ---")
 	quickSortArr := []int{5,3,8,1,3,9,57,4,28,13,2,142,7}
 	QuickSort(quickSortArr, 0,len(quickSortArr)-1)
 	fmt.Println(quickSortArr)
