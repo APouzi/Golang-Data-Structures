97a92fee68019b4c9c22c092a3ce0590d0012b46
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-02 06:23:54 +0900

    shuffle string has been added -AP

--- RandomLC/RunRandomLC.go
+++ RandomLC/RunRandomLC.go
@@ -1,1 +1,12 @@
 package randomlc
+
+// You are given a string s and an integer array indices of the same length. The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+
+// Return the shuffled string.
+
+//  s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+// Output: "leetcode"
+
+func ShuffleString(s string, indices []int){
+	
+}
