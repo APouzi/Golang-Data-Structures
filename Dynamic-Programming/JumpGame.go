fa8ac42bb6d6858ba2d9fc2c0cbe0f231219f234
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-02 20:01:22 +0900

    This has allowed me to add the JumpGame dynamic programming approach -AP

--- Dynamic-Programming/JumpGame.go
+++ Dynamic-Programming/JumpGame.go
@@ -1,1 +1,12 @@
 package dynamicprogramming
+
+func canJump(nums []int) bool {
+	var LastGoodIndex int = len(nums) - 1
+	var size int = len(nums) - 1
+	for i := size; i >= 0; i-- {
+		if i+nums[i] >= LastGoodIndex {
+			LastGoodIndex = i
+		}
+	}
+	return LastGoodIndex == 0
+}
