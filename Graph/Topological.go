64034bb5b1d50de9b390bf487b1eee50a9fed7b9
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-03 05:33:03 +0900

    Added the problem statement for the array version of the topological graph -AP

--- Graph/Topological.go
+++ Graph/Topological.go
@@ -177,7 +177,11 @@ func TopologicalDFS(curr int, graph map[int][]int, seen map[int]int, topo *[]int
 	return true
 }
 
-// Lets do an array version of all of this
+// Lets do an array version of all of this: Called CourseSchedule:
+// There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+// Return true if you can finish all courses. Otherwise, return false.
+// Input:  numCourses = 2, prerequisites = [[1,0],[0,1]]
+// Output: False
 func TopologicalSortArr(arr [][]int){
 	var graph map[int][]int = make(map[int][]int)
 
