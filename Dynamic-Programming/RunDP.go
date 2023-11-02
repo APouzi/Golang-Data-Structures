3393406bb2ecf42b5273bc09d96b4918c9d05570
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-02 20:05:25 +0900

    running Dynamic Programming is finalized -AP

--- Dynamic-Programming/RunDP.go
+++ Dynamic-Programming/RunDP.go
@@ -1,5 +1,11 @@
 package dynamicprogramming
 
-func Run() {
+import "fmt"
 
+func RunDP() {
+	// ---Can Jump Problem---
+	jumpTrue := []int{2, 3, 1, 1, 4}
+	// jumpFalse := []int{3, 2, 1, 0, 4}
+	canit := canJump(jumpTrue)
+	fmt.Println("canJump can jump",canit)
 }
