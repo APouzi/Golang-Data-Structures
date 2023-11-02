fb2473e6def872bf16c4bbf33b76b17276bc48f9
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-02 20:08:47 +0900

    small print statement change -AP

--- Dynamic-Programming/RunDP.go
+++ Dynamic-Programming/RunDP.go
@@ -3,6 +3,7 @@ package dynamicprogramming
 import "fmt"
 
 func RunDP() {
+	fmt.Println("\nDynamic Programming has started")
 	// ---Can Jump Problem---
 	jumpTrue := []int{2, 3, 1, 1, 4}
 	// jumpFalse := []int{3, 2, 1, 0, 4}
