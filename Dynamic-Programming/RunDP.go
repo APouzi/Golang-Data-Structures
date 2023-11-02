72972746edca32a77b1c504e3eab991e1334b9e8
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-03 04:53:53 +0900

    fixed a print out statement -AP

--- Dynamic-Programming/RunDP.go
+++ Dynamic-Programming/RunDP.go
@@ -8,5 +8,5 @@ func RunDP() {
 	jumpTrue := []int{2, 3, 1, 1, 4}
 	// jumpFalse := []int{3, 2, 1, 0, 4}
 	canit := canJump(jumpTrue)
-	fmt.Println("canJump can jump",canit)
+	fmt.Println("JumpGame:",canit)
 }
