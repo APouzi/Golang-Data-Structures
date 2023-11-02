e64f91c4f952f9d411bb510ad30f124f5a366180
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-03 04:50:20 +0900

    added some failed and pass cases for this to run

--- Stack/runStack.go
+++ Stack/runStack.go
@@ -1,1 +1,12 @@
 package stack
+
+import "fmt"
+
+func RunStack() {
+	fmt.Println("\nisValid started")
+	// s := "()" //Pass
+	s := "(())[]" //Pass
+	// s := "){" //Fail
+	// s:= "[(([))]]" //Fail
+	fmt.Println(isValid(s))
+}
