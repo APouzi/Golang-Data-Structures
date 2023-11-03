bd53e698db79452da1c3c8aa569f3e6904ea9466
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-04 07:12:31 +0900

    graph titles -AP

--- Graph/GraphRepresentations.go
+++ Graph/GraphRepresentations.go
@@ -1,15 +1,18 @@
 package graph
 
+// ---Graph Adjacency---
 // Sometimes we don't have nodes that are representing multivariable/multidimensional data structures. This means that we only have values like numbers, letters, symbols or what have you
 type GraphAdj struct {
 	Vertices map[int][]int
 }
 
+// ---Graph Node Adjacency---
 // This one is usually best because of O(1) loop up time plus ability to loop over them as we are doing out path searches. 
 type GraphNodeAdj struct {
 	Vertices map[*GraphNode][]*Node
 }
 
+//---Matrix Graph ---
 // Here we have the matrix that is representing a bidirectional graphs, which are technically acyclic. Which means that finding the solution to a path would mean that it's going to give us an extremely large number of possible paths. 
 // Note: There is two versions of this, one is where the graph is representing the nodes themselves. An example is like this where we have the 1's representing the nodes and fact they are next to one another means they are together.
 // [1,1,1,0]
@@ -24,6 +27,7 @@ type GraphArr struct {
 	Matrix [][]int
 }
 
+// ---Node Graph using Array ---
 // This is where we have a list of Graph nodes and the edges that we are being represented. We loop over the vertices and insert those looped over nodes into the edges.
 type NodeGraph struct {
 	Vertices []*GraphNode
