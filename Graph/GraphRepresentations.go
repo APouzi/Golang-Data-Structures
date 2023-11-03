6b70a8dd0b90430e3a1cc45646b37365c80c1f8d
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-04 06:57:59 +0900

    Here we added all the graph representations when we can possibly have -AP

--- Graph/GraphRepresentations.go
+++ Graph/GraphRepresentations.go
@@ -1,2 +1,28 @@
 package graph
 
+type GraphAdj struct {
+	Vertices map[int][]int
+}
+
+type GraphNodeAdj struct {
+	Vertices map[*GraphNode][]*Node
+}
+
+type GraphArr struct {
+	Matrix [][]int
+}
+
+type GraphNodes struct {
+	Vertices []*GraphNode
+	Edges    []*Edges
+}
+
+// Utility Componenets
+type Edges struct {
+	To   *GraphNode
+	From *GraphNode
+}
+
+type GraphNode struct {
+	Val int
+}
