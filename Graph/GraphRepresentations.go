191804aef2d2713db7fa9cbc5622cb0d1e1b706b
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-04 06:58:45 +0900

    Added comments explaining the graph represntations -AP

--- Graph/GraphRepresentations.go
+++ Graph/GraphRepresentations.go
@@ -1,13 +1,16 @@
 package graph
 
+// Sometimes we don't have nodes that are representing multivariable/multidimensional data structures. This means that we only have values like numbers, letters, symbols or what have you
 type GraphAdj struct {
 	Vertices map[int][]int
 }
 
+// This one is usually best because of O(1) loop up time plus ability to loop over them as we are doing out path searches. 
 type GraphNodeAdj struct {
 	Vertices map[*GraphNode][]*Node
 }
 
+// Here we have the matrix that is representing a bidirectional graphs, which are technically acyclic. Which means that finding the solution to a path would mean that it's going to give us an extremely large number of possible paths
 type GraphArr struct {
 	Matrix [][]int
 }
