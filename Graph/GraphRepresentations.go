789e5968b89f30fef390f85bb46d2d79997c14d4
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-04 07:07:13 +0900

    Here we have the representations of matrix array graph being properly represented -AP

--- Graph/GraphRepresentations.go
+++ Graph/GraphRepresentations.go
@@ -10,7 +10,16 @@ type GraphNodeAdj struct {
 	Vertices map[*GraphNode][]*Node
 }
 
-// Here we have the matrix that is representing a bidirectional graphs, which are technically acyclic. Which means that finding the solution to a path would mean that it's going to give us an extremely large number of possible paths
+// Here we have the matrix that is representing a bidirectional graphs, which are technically acyclic. Which means that finding the solution to a path would mean that it's going to give us an extremely large number of possible paths. 
+// Note: There is two versions of this, one is where the graph is representing the nodes themselves. An example is like this where we have the 1's representing the nodes and fact they are next to one another means they are together.
+// [1,1,1,0]
+// [1,1,1,0]
+// [1,0,0,0]
+
+// The other one is where we have the x,y is going to be representing the nodes and the integers inside are the edges. Meaning that if in the example below (1 > 0) and (0 > 1) are connected to each other. 
+// [0,1,1]
+// [1,0,1]
+// [1,0,0]
 type GraphArr struct {
 	Matrix [][]int
 }
