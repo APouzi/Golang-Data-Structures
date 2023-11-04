1e23838d128965d19a39f695ffd2a753fe86508b
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-05 05:22:09 +0900

    reverse a set of group that allows us to be reverse k group of nodes -AP

--- LinkedList/ReverseLinkedList.go
+++ LinkedList/ReverseLinkedList.go
@@ -26,3 +26,23 @@ func LLRev(currNode *LinkedNode) *LinkedNode { // 6,5,4,3,2,1
 	return returnedHead
 
 }
+
+func LLRevKSendOff(curr *LinkedNode, k int) *LinkedNode {
+	sendBack, connectTo, connect := LLRevK(curr, k, 1)
+	connect.Next = connectTo
+	return sendBack
+}
+
+func LLRevK(curr *LinkedNode, k, flag int) (*LinkedNode, *LinkedNode, *LinkedNode) {
+
+	if flag == k || curr.Next == nil {
+		return curr, curr.Next, nil
+	}
+
+	conn, retNext, _ := LLRevK(curr.Next, k, flag+1)
+	curr.Next.Next = curr
+	curr.Next = nil
+	return conn, retNext, curr
+}
+
+// 1,2,3,4,5,6
