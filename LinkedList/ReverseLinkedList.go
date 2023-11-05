863f66de07371cf95ec10cfdf13fa9ec9aa66a43
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-06 04:53:12 +0900

    Added even more comments explaining the reveresal stuff -AP

--- LinkedList/ReverseLinkedList.go
+++ LinkedList/ReverseLinkedList.go
@@ -1,7 +1,5 @@
 package LinkedList
 
-import "fmt"
-
 func RevRecur(node *LinkedNode) *LinkedNode {
 	if node.Next == nil {
 
@@ -48,13 +46,17 @@ func LLRevK(curr *LinkedNode, k, flag int) (*LinkedNode, *LinkedNode, *LinkedNod
 	return conn, retNext, curr
 }
 
+// 1) loop through and put every kth node into the stack of the parameter of the head, make sure to build up the count, that will be the size of k
+// 2) If the loop reaches nil of the list, we must return the nil, which will exit out the recursion and returnThis return will act as the previous to assign to as next
+// When we peel off the stack, we will have access to the variables at each recursion.
+// 3) As we peel back, this is where we reverse the linked nodes. At each peelback iteration, we do up to K, we do this by subtracting the incremented count.
+// 4) return the newly assigned prev, which will be the current head, before we assign the head being the tempNext.
 // 9  8  7  6  5  4  3  2  1	>>>		7  8  9 | 4  5  6 | 1  2  3
 func ReverseKGroups(head *LinkedNode, k int) *LinkedNode {
 	var copyCurr *LinkedNode = head
 	var count int = 0
 	for count < k { //This loop is going to be traverse until linked list reaches the end
 		if copyCurr == nil { // When we return curr(3), this actually exit's out the recursion and start the peel back of the stack.
-			fmt.Println(head)
 			return head //With the LL above, this will return "3".
 		}
 		copyCurr = copyCurr.Next
@@ -63,7 +65,7 @@ func ReverseKGroups(head *LinkedNode, k int) *LinkedNode {
 	var last *LinkedNode = ReverseKGroups(copyCurr, k) //The first return will be 3
 	// last = nil, last = 1, last = 4
 	// head = 3, head = 6, head = 9
-	for count > 0 { 
+	for count > 0 {
 		next := head.Next // 2, 1, nil
 		head.Next = last  // 3 > nil, 2 > 3, 1 > 2
 		last = head       // nil = 3, 3 = 2, 2 = 1
