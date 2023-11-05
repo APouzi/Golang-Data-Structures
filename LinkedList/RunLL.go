97e21237b72764c7d0fe20394ee0a20e354ab971
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-05 09:12:47 +0900

    LinkedList running this -AP

--- LinkedList/RunLL.go
+++ LinkedList/RunLL.go
@@ -1,5 +1,25 @@
 package LinkedList
 
-func RunLinkedList(){
-	
+import "fmt"
+
+func RunLinkedList() {
+	LL := LinkedList{}
+	for i := 1; i < 10; i++ {
+		LL.AddNode(i)
+	}
+
+	L2 := LinkedList{}
+	for i := 1; i < 10; i++ {
+		L2.AddNode(i)
+	}
+	fmt.Println("ReverseKGroups")
+	LL.PrintList()
+	reved := ReverseKGroups(LL.Head, 3)
+	println("\n print reverse:")
+	PrintAfterReverse(reved)
+
+
+	// newHead := revRecur(LinkedList.head)
+	// RemoveDup(LL.Head)
+
 }
