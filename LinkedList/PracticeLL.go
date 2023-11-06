793457b7b82d1f6615ce740f476aea5371738970
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-06 19:31:27 +0900

    added the practice functions before adding it to gitignore -AP

--- LinkedList/PracticeLL.go
+++ LinkedList/PracticeLL.go
@@ -1,6 +1,42 @@
 package LinkedList
 
-// 1, 2, 3, 4, 5, 6, 7, 8, 9
+// The List passed in: 9, 8, 7, 6, 5, 4, 3, 2, 1
 func ReverseKGroupsPrac(head *LinkedNode, k int) *LinkedNode {
+	return head
+}
+
+func RemoveDupPrac(head *LinkedNode)*LinkedNode{
+	return head
+}
+
+func MiddleNodeofLLPrac(head *LinkedNode) *LinkedNode{
+	return head
+}
+
+func RevLLIterativePrac(head *LinkedNode)*LinkedNode{
+	return head
+}
+
+func RevLLRecurPrac(head *LinkedNode)*LinkedNode{
+	return head
+}
+
+func CycleDetectionPrac(head *LinkedNode)*LinkedNode{
+	return head
+}
+
+func ReorderListPrac(head *LinkedNode)*LinkedNode{
+	return head
+}
+
+func ReverseLinkedList2Prac(head *LinkedNode)*LinkedNode{
+	return head
+}
+
+func SwappingNodesInLLPrac(head *LinkedNode)*LinkedNode{
+	return head
+}
+
+func SwapNodesInPairsPrac(head *LinkedNode)*LinkedNode{
 	return head
 }
