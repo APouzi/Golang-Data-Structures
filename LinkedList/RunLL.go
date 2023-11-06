02e63f09a962569f4fa35f5a5ab867ef6d13691b
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-06 19:10:19 +0900

    minized the code as I made the creation of the LinkedList instead of looping over stuff over and over -AP

--- LinkedList/RunLL.go
+++ LinkedList/RunLL.go
@@ -3,22 +3,20 @@ package LinkedList
 import "fmt"
 
 func RunLinkedList() {
-	LL := LinkedList{}
-	for i := 1; i < 10; i++ {
-		LL.AddNode(i)
-	}
+	LL := CreateLinkedList()
+	L2 := CreateLinkedList()
 
-	L2 := LinkedList{}
-	for i := 1; i < 10; i++ {
-		L2.AddNode(i)
-	}
 	fmt.Println("ReverseKGroups")
 	LL.PrintList()
 	reved := ReverseKGroups(LL.Head, 3)
 	println("\n print reverse:")
 	PrintAfterReverse(reved)
-
-
+	
+	fmt.Println("\n")
+	fmt.Println(L2.Head.Val)
+	revedPrac := LLRevIterative(L2.Head)
+	println("\n print reverse Prac:")
+	PrintAfterReverse(revedPrac)
 	// newHead := revRecur(LinkedList.head)
 	// RemoveDup(LL.Head)
 
