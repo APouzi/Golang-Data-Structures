94b20b99ccc7513569fb688e2698b6f25e55620d
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-07 18:21:14 +0900

    comments updated -AP

--- Sorting/QuickSort.go
+++ Sorting/QuickSort.go
@@ -5,7 +5,7 @@ func QuickSort(arr []int, low, high int) {
 
 		pivot := Partition(arr, low, high) //The pivot, like mid in merge is going to be the divider, except that the pivot will be in the correct place when done so we need to use this as the divider and get the pivot (+-1) and recuservly do this until we are done.
 
-		// Unlike mergesort, the actual array isn't being divided and then merged together into a new array. This is just getting the source array and the indexs will be altering or "dividing" until we reach elements that are the size of one. Once they are that size, they are technically completed
+		//The actual array isn't being divided and then merged together into a new array. This is just getting the source array and the indexs will be altering or "dividing" until we reach elements that are the size of one. Once they are that size, they are technically completed
 		QuickSort(arr, low, pivot-1)
 		QuickSort(arr, pivot+1, high)
 	}
@@ -24,7 +24,7 @@ func Partition(arr []int, low, high int) int {
 	return greater
 }
 
-// In textbooks it's like this for some reason,
+// In textbooks it's like this for some reason, 
 func LumotoPartition(arr []int, low, high int) int {
 	greater := low - 1
 	pivot := arr[high]
