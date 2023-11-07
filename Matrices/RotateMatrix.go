faf78a86f625ddbbcfaa341db7070baca35b5203
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-08 08:40:39 +0900

    finalized my algorithim for the rotation of the matrix -AP

--- Matrices/RotateMatrix.go
+++ Matrices/RotateMatrix.go
@@ -10,3 +10,19 @@ package matrices
 //swap the top left cell with the bottom left cell
 //Move to the next group of four cells and repeat the above steps
 
+func RotateMatrix(arr [][]int) [][]int{
+
+	quadrants := len(arr)/2
+	for i:=0; i < quadrants; i++{
+		var RowStart, ColStart, RowEnd, ColEnd int = 0+i,0+i,len(arr)-1-i,len(arr[0])-1-i
+		var rotations int = len(arr)-1-(i*2)
+		for j:=0; j<rotations;j++{
+			topLeft := arr[RowStart][ColStart+j]
+			arr[RowStart][ColStart+j] = arr[RowEnd-j][ColStart]
+			arr[RowEnd-j][ColStart] = arr[RowEnd][ColEnd-j]
+			arr[RowEnd][ColEnd-j] = arr[RowStart+j][ColEnd]
+			arr[RowStart+j][ColEnd] = topLeft
+		}
+	}
+	return arr
+}
