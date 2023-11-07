6197420578598532bbc58491f15dcf43d44e8452
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-07 17:35:44 +0900

    moved spiral matrix to matrix folder and intialized it's run -AP

--- Matrices/SpiralMatrix.go
+++ Matrices/SpiralMatrix.go
@@ -1,4 +1,4 @@
-package randomlc
+package matrices
 
 import "fmt"
 
--- Matrices/runMatrices.go
+++ Matrices/runMatrices.go
@@ -1,1 +1,11 @@
 package matrices
+
+func RunMatrix() {
+	matrix := [][]int{
+		{1, 2, 3, 4},
+		{12, 13, 14, 5},
+		{11, 16, 15, 6},
+		{10, 9, 8, 7},
+	}
+	SpiralMatrix(matrix)
+}
