ac03312004746d886d60e8a70dcf4334f34863b1
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-08 09:05:07 +0900

    setZeros has been updated -AP

--- Matrices/SetMatrixZeros.go
+++ Matrices/SetMatrixZeros.go
@@ -3,7 +3,7 @@ package matrices
 // Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's. You must do it in place.
 //Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
 //Output: [[1,0,1],[0,0,0],[1,0,1]]
-func setZeroes(matrix [][]int) {
+func setZeroes(matrix [][]int) [][]int {
 	rowEnd, colEnd := len(matrix)-1, len(matrix[0])-1
 	var col map[int]bool = map[int]bool{}
 	var row map[int]bool = map[int]bool{}
@@ -23,4 +23,5 @@ func setZeroes(matrix [][]int) {
 			}
 		}
 	}
+	return matrix
 }
