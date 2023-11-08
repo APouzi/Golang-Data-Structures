0ff5b75bbb28ac029a6745f6565eb85bdc49955f
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-09 05:50:38 +0900

    added the appropriate code for mirror matrix -AP

--- Matrices/MirrorMatrix.go
+++ Matrices/MirrorMatrix.go
@@ -1,2 +1,12 @@
 package matrices
 
+func MirrorMatrixLeftToRight(arr [][]int) [][]int {
+	var colStart, rowStart, colEnd int = 0, 0, len(arr[0])-1
+	var colLim, rowLim int = len(arr[0])/2 , len(arr)
+	for i := 0; i < colLim; i++ {
+		for j := 0; j < rowLim; j++ {
+			arr[rowStart+j][colStart+i], arr[rowStart+j][colEnd-i] = arr[rowStart+j][colEnd-i], arr[rowStart+j][colStart+i]
+		}
+	}
+	return arr
+}
