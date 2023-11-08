11a1ea2597d2eeb5f6b65f5e1312f8ae521fa990
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-08 09:00:22 +0900

    run matrices has been updated for rotate image -AP

--- Matrices/runMatrices.go
+++ Matrices/runMatrices.go
@@ -1,5 +1,7 @@
 package matrices
 
+import "fmt"
+
 func RunMatrix() {
 	matrix := [][]int{
 		{1, 2, 3, 4},
@@ -8,4 +10,12 @@ func RunMatrix() {
 		{10, 9, 8, 7},
 	}
 	SpiralMatrix(matrix)
+	
+	fmt.Println("Spiral Matrix Practice:",SpiralMatrixPrac(matrix))
+	matrix2 := [][]int{
+		{1, 2, 3,},
+		{12, 13, 5},
+		{10, 9, 7},
+	}
+	fmt.Println("Rotate Matrix:", RotateMatrix(matrix2))
 }
