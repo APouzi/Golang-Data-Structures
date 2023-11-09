7d5d0d0f682b6ddb1d29e065d6aad5dde2f66177
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-09 09:09:00 +0900

    added where the ball drops run -AP

--- Matrices/runMatrices.go
+++ Matrices/runMatrices.go
@@ -20,18 +20,26 @@ func RunMatrix() {
 	fmt.Println("Rotate Matrix:", RotateMatrix(matrix2))
 
 	matrix3 := [][]int{
-		{1, 2, 3,},
-		{12, 0, 5},
-		{10, 9, 7},
+		{ 1,  2, 3,},
+		{12, 0,  5},
+		{10, 9,  7},
 	}
 
 	fmt.Println("Set Zeros",setZeroes(matrix3))
 
 	matrix4 := [][]int{
-		{1, 2,3, 3,},
-		{12, 0,4, 5},
-		{10, 9,7, 7},
+		{ 1, 2, 3, 3,},
+		{12, 0, 4, 5},
+		{10, 9, 7, 7},
 	}
 	fmt.Println("mirror matrix:", MirrorMatrixLeftToRight(matrix4))
 	
+	matrixBall := [][]int{
+		{1,1,1,-1,-1},
+		{1,1,1,-1,-1},
+		{-1,-1,-1,1,1},
+		{1,1,1,1,-1},
+		{-1,-1,-1,-1,-1}}
+
+	fmt.Println("where will the ball drop?",whereTheBallDrops(matrixBall))
 }
