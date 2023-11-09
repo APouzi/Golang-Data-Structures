b6a0067ada91f9e2078988eb6985f382c8898e2b
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-10 06:21:46 +0900

    added some helpful comments explaining the process of Insertion sort -AP

--- Sorting/InsertionSort.go
+++ Sorting/InsertionSort.go
@@ -1,11 +1,16 @@
 package sorting
 
+//Best case O(n)
+//average O(n2)
+//worst case O(n2)
 func InsertionSortAlgo(arr []int) {
 	var sorted int
 	var key int
 	for i := 1; i < len(arr); i++ {
 		sorted = i - 1
 		key = arr[i]
+		// I ran into a problem before where I had just used a singular "if statement" (for key < arr[sorted]) instead of putting it inside of the for loop, this wont work because sometimes we have to cut this shift over short. example:
+		//7,6,3,1,5,4, key = 5, i = 1. What happens with this is that key will have to stop when the 3 shifts to the right and 5 needs to take it's place and not go further.
 		for sorted >= 0 && key < arr[sorted] {
 			arr[sorted+1] = arr[sorted]
 			//This is shifting everything to the right. The first to change will always be the key that will be shifted into. This is going to be
