79b2cf07b2f3fd97624012c65693eb44f3dc1434
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-10 01:44:06 +0900

    This is going to be some calls for runMatrices -AP

--- LinkedList/RunLL.go
+++ LinkedList/RunLL.go
@@ -21,11 +21,33 @@ func RunLinkedList() {
 
 	fmt.Println("\nReverse K Groups Linked List:")
 	L3.PrintList()
-	revedPrac2 := ReverseKGroupsPrac(L3.Head, 3)
+	reved2 := ReverseKGroups(L3.Head, 3)
 	println("\nprint reverse:")
-	PrintAfterReverse(revedPrac2)
+	PrintAfterReverse(reved2)
 	// newHead := revRecur(LinkedList.head)
 	// RemoveDup(LL.Head)
+
+	fmt.Println("\n\n---LinkedList Practice has started---")
+	L4 := CreateLinkedList()
+	fmt.Println("Reverse K Groups Practice Linked List:")
+	L4.PrintList()
+	revedPrac2 := ReverseKGroupsPrac(L4.Head, 3)
+	println("\nprint reverse:")
+	PrintAfterReverse(revedPrac2)
+
+	L5 := CreateLinkedList()
+	fmt.Println("\nLinkedList Middle Node:", MiddleNodeofLLPrac(L5.Head))
+
+	L6 := CreateLinkedList()
+	fmt.Println("LinkedList Reversal Recursive Prac:")
+	PrintAfterReverse(RevLLRecurPrac(L6.Head))
+
+	LCycle := CreateCycleLinkedList()
+	fmt.Println("\nLinkedList Cycle:", CycleDetectionPrac(LCycle.Head))
+
+	L7 := CreateLinkedList()
+	fmt.Println("\nLinked List Reverse Iterative Practice:")
+	PrintAfterReverse(RevLLIterativePrac(L7.Head))
 
 }
 
@@ -34,5 +56,14 @@ func CreateLinkedList() *LinkedList{
 	for i := 1; i < 10; i++ {
 		list.AddNode(i)
 	}
+	return &list
+}
+
+func CreateCycleLinkedList() *LinkedList{
+	list := LinkedList{}
+	for i := 1; i < 10; i++ {
+		list.AddNode(i)
+	}
+	list.MakeCycle()
 	return &list
 }
