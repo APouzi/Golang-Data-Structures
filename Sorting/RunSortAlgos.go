d169dfe413ab2a708918d506b77562480649120a
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-11 05:08:35 +0900

    sorting run file has better formatting -AP

--- Sorting/RunSortAlgos.go
+++ Sorting/RunSortAlgos.go
@@ -15,32 +15,26 @@ func RunSorting() {
 	// ----BubbleSort----
 	BubbleSort(BubbleSortNums)
 	fmt.Println("\nBubbleSort",BubbleSortNums)
-	BubbleSortNumsPrac := []int{9,8,7,6,5,4,11,12,34,123,565,3123,123,1236,64,7654,124,1235,643,3,2,1,67}
-	BubbleSort2(BubbleSortNumsPrac)
-	fmt.Println("Practice BubbleSort",BubbleSortNumsPrac)
+	BubbleSortNums2 := []int{9,8,7,6,5,4,11,12,34,123,565,3123,123,1236,64,7654,124,1235,643,3,2,1,67}
+	BubbleSort2(BubbleSortNums2)
+	fmt.Println("Practice BubbleSort",BubbleSortNums2)
 
 	//--- Insertion Sort ---
 	InsertionSortNums := []int{9,8,7,6,5,4,3,2,1,11,12,34,64,7654,123,1236,124,1235,643,67,123,565,3123}
 	InsertionSortAlgo(InsertionSortNums)
 	fmt.Println("\nInsertion Sort",InsertionSortNums)
-	InsertionSortNumsPrac := []int{9,8,7,6,5,4,3,2,1,11,12,34,64,7654,123,1236,124,1235,643,67,123,565,3123}
-	InsertionSortPrac(InsertionSortNumsPrac)
-	fmt.Println("Insertion Sort Practice",InsertionSortNumsPrac)
+
 
 	// ----Selection Sort----
 	SelectionSortNums := []int{9,8,7,6,5,4,3,2,1,11,12,34,64,7654,123,1236,124,1235,643,67,123,565,3123}
 	SelectionSortAlgo(SelectionSortNums)
 	fmt.Println("SelectionSort",SelectionSortNums)
-	SelectionSortNumsPrac := []int{9,8,7,6,5,4,3,2,1,11,12,34,64,7654,123,1236,124,1235,643,67,123,565,3123}
-	SelectionSortPrac(SelectionSortNumsPrac)
-	fmt.Println("SelectionSort Practice",SelectionSortNumsPrac)
+	
 
 	// ----------Merge Sort (Array and Linked List)----------
 	fmt.Println("\n---Merge Sort Algorithm has begun---")
-	// MergeSortNum := []int{9,8,7,6,5,4,3,2,1,11,12,34,64,7654,123,1236,124,1235,643,67,123,565,3123}
 	MergeSortNum := []int{6,5,12,10,9,1}
 	MergeSort(MergeSortNum, 0, len(MergeSortNum)-1)
-	// sortedPrac := MergeSortPrac(MergeSortNum)
 	fmt.Println("MergeSort Algorithm Result",MergeSortNum)
 	fmt.Println("---Merge Sort Algorithm 2 has begun---")
 	MergeSortNum2 := []int{5,3,4,1,2}
@@ -48,13 +42,10 @@ func RunSorting() {
 	fmt.Println("MergeSort2 Algorithm Result", arr1)
 
 	fmt.Println("\n--- Merge Sort Algorithm LinkedList has begun ---")
-
 	head, _ := createLinkedListUnsorted()
 	fmt.Println("Original linked list:")
 	printLinkedList(head)
-
 	sortedHead := MergeSortLinkedList(head)
-
 	fmt.Println("Sorted linked list:")
 	printLinkedList(sortedHead)
 
@@ -63,10 +54,26 @@ func RunSorting() {
 	quickSortArr := []int{5,3,8,1,3,9,57,4,28,13,2,142,7}
 	QuickSort(quickSortArr, 0,len(quickSortArr)-1)
 	fmt.Println(quickSortArr)
-	fmt.Println("QuickSort Practice")
+
+	BubbleSortNumsPrac := []int{9,8,7,6,5,4,11,12,34,123,565,3123,123,1236,64,7654,124,1235,643,3,2,1,67}
+	BubbleSort2(BubbleSortNumsPrac)
+	fmt.Println("Practice BubbleSort",BubbleSortNumsPrac)
+
+	SelectionSortNumsPrac := []int{9,8,7,6,5,4,3,2,1,11,12,34,64,7654,123,1236,124,1235,643,67,123,565,3123}
+	SelectionSortPrac(SelectionSortNumsPrac)
+	fmt.Println("Selection Sort Practice",SelectionSortNumsPrac)
+
+	InsertionSortNumsPrac := []int{9,8,7,6,5,4,3,2,1,11,12,34,64,7654,123,1236,124,1235,643,67,123,565,3123}
+	InsertionSortPrac(InsertionSortNumsPrac)
+	fmt.Println("Insertion Sort Practice",InsertionSortNumsPrac)
+
+	MergeSortNumPrac := []int{9,8,7,6,5,4,3,2,1,11,12,34,64,7654,123,1236,124,1235,643,67,123,565,3123}
+	sortedPrac := MergeSortPrac(MergeSortNumPrac)
+	fmt.Println("Merge Sort Prac:",sortedPrac)
+
 	partitionSortArr := []int{5,3,8,1,3,9,57,4,28,13,2,142,7}
-	QuickSort(partitionSortArr, 0 , len(partitionSortArr)-1)
-	fmt.Println(partitionSortArr)
+	QuickSortPrac(partitionSortArr, 0 , len(partitionSortArr)-1)
+	fmt.Println("QuickSort Practice",partitionSortArr)
 
 }
 
