651180fe0cd4d45f97a58e00ad464dd002f5edd4
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-11 05:34:18 +0900

    updated sort algo -AP

---
+++ Sorting/PracticeSortAlgos.go
@@ -0,0 +1,34 @@
+package sorting
+
+func BubbleSortPrac(arr []int) {
+
+}
+
+func SelectionSortPrac(arr []int) {
+
+}
+
+func InsertionSortPrac(arr []int) {
+
+}
+
+func MergeSortPrac(arr []int) []int {
+	return []int{}
+}
+
+func QuickSortPrac(arr []int, low, high int) []int {
+	return []int{}
+}
+
+// 5,3,8,1,3,9,57,4,28,13,2,142,7
+func ParititionSort(arr []int, low, high int) int {
+	return -1
+}
+
+func HeapSortPrac(arr []int) {
+
+}
+
+func RadixSort(arr []int) {
+
+}
