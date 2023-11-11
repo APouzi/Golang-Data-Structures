618ea85ae9199d83c1b29fa6ef8a1125eaf0dc29
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-12 05:37:48 +0900

    added some practice stuff for algorithm sorting practice -AP

--- Sorting/PracticeSortAlgos.go
+++ Sorting/PracticeSortAlgos.go
@@ -16,12 +16,20 @@ func MergeSortPrac(arr []int) []int {
 	return []int{}
 }
 
+func MergeSortPrac2(arr []int, st, end int) []int {
+	return []int{}
+}
+
+func MergeSortPracLinkedList(head *Node) *Node {
+	return head
+}
+
 func QuickSortPrac(arr []int, low, high int) []int {
 	return []int{}
 }
 
 // 5,3,8,1,3,9,57,4,28,13,2,142,7
-func ParititionSort(arr []int, low, high int) int {
+func Paritition(arr []int, low, high int) int {
 	return -1
 }
 
@@ -29,6 +37,6 @@ func HeapSortPrac(arr []int) {
 
 }
 
-func RadixSort(arr []int) {
+func RadixSortPrac(arr []int) {
 
 }
