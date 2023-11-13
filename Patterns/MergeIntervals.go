eeaffc428aa52379842b2c839c2a0d4cadede995
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-14 05:06:00 +0900

    added merge interval, the optimized version of this -AP

--- Patterns/MergeIntervals.go
+++ Patterns/MergeIntervals.go
@@ -3,6 +3,54 @@ package patterns
 import "fmt"
 
 //Note, DO NOT use quicksort for intervals, this is because quicksort intervals is unstable.
+func MergeIntervals(intervals [][]int) [][]int {
+	sortedIntervals := MergeSortInterval(intervals, 0, len(intervals)-1)
+	fmt.Println(sortedIntervals)
+	merged := [][]int{}
+	// var newPair []int
+	for i := 0; i<len(sortedIntervals);i++{
+		//(1,3),(2,6)
+		if len(merged) == 0 || merged[len(merged)-1][1] < sortedIntervals[i][0]{
+			// newPair = []int{sortedIntervals[i][0],sortedIntervals[i+1][1]}
+			merged =append(merged, sortedIntervals[i])
+		}else{
+			merged[len(merged)-1][1] = max(merged[len(merged)-1][1],sortedIntervals[i][1])
+		}
+		// if sortedIntervals[i][1] >= sortedIntervals[i+1][0]{
+		// 	// newPair = []int{sortedIntervals[i][0],sortedIntervals[i+1][1]}
+		// }
+	}
+	return merged
+}
+
+func max(a, b int) int{
+	if a > b{
+		return a
+	}
+	return b
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 func MergeIntervalsAttempt(intervals [][]int) [][]int {
 	sortedIntervals := MergeSortInterval(intervals, 0, len(intervals)-1)
 	fmt.Println(sortedIntervals)
@@ -21,6 +69,4 @@ func MergeIntervalsAttempt(intervals [][]int) [][]int {
 		}
 	}
 	return listAns
-}
-
 }
