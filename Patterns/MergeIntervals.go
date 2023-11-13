74758ead18e50e6df20aedc24492411fe64bfe54
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-14 04:58:56 +0900

    added my goofy attempt at merge intervals -AP

--- Patterns/MergeIntervals.go
+++ Patterns/MergeIntervals.go
@@ -1,5 +1,26 @@
 package patterns
 
-func MergeIntervals([][]int) []int {
-	return []int{}
+import "fmt"
+
+//Note, DO NOT use quicksort for intervals, this is because quicksort intervals is unstable.
+func MergeIntervalsAttempt(intervals [][]int) [][]int {
+	sortedIntervals := MergeSortInterval(intervals, 0, len(intervals)-1)
+	fmt.Println(sortedIntervals)
+	listAns := [][]int{}
+	var newPair []int
+	for i := 0; i<len(sortedIntervals)-1;i++{
+		//(1,3),(2,6)
+		if sortedIntervals[i][1] >= sortedIntervals[i+1][0]{
+			newPair = []int{sortedIntervals[i][0],sortedIntervals[i+1][1]}
+		}
+		if len(listAns) > 0 && newPair[0] == listAns[len(listAns)-1][0] && listAns[len(listAns)-1][1] < newPair[1]{
+			listAns = listAns[:len(listAns)-1]
+			listAns = append(listAns, newPair)
+		} else if len(listAns) == 0 || newPair[0] != listAns[len(listAns)-1][0]{
+			listAns = append(listAns, newPair)
+		}
+	}
+	return listAns
+}
+
 }
