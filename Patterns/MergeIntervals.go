e6cf36ef39684930bc059abffac901506ecae9ce
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-14 20:03:36 +0900

    changed first and second to 1st and 2nd for better memory retention -AP

--- Patterns/MergeIntervals.go
+++ Patterns/MergeIntervals.go
@@ -19,14 +19,14 @@ func MergeIntervals(intervals [][]int) [][]int {
 	var sortedIntervals [][]int = MergeSortInterval(intervals, 0, len(intervals)-1)
 	fmt.Println(sortedIntervals)
 	var merged [][]int = make([][]int,0)
-	//No matter what, we need to add the first interval
+	//No matter what, we need to add the 1st interval
 	merged = append(merged, sortedIntervals[0])
 	for i := 1; i<len(sortedIntervals);i++{
-		//In this instance, we are asking if the pair's second element is smaller than the first element in the next pair, then we add it. This just implies we have a disjointed set.
+		//In this instance, we are asking if the pair's 2nd element is smaller than the 1st element in the next pair, then we add it. This just implies we have a disjointed set.
 		if merged[len(merged)-1][1] < sortedIntervals[i][0]{
 			merged =append(merged, sortedIntervals[i])
 		}else{
-			//Here we found that the two pairs are actually joined because the 2nd element in the first pair is in fact bigger than the first element in the next pair, but need to figure out if we should merge them by comparing both pair's 2nd elements and seeing which one is bigger. Remember that [1,4],[4,5] are overlapping
+			//Here we found that the two pairs are actually joined because the 2nd element in the 1st pair is in fact bigger than the 1st element in the next pair, but need to figure out if we should merge them by comparing both pair's 2nd elements and seeing which one is bigger. Remember that [1,4],[4,5] are overlapping
 			merged[len(merged)-1][1] = max(merged[len(merged)-1][1],sortedIntervals[i][1])
 		}
 	}
