4c4774dd257dc0c45498a334cbd1f5824f808b24
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-15 05:23:14 +0900

    added some notes for practice in patterns to describe problem -AP

--- Patterns/PatternPractice.go
+++ Patterns/PatternPractice.go
@@ -1,5 +1,18 @@
 package patterns
 
+// Write an algorithm to determine if a number n is happy.
+// A happy number is a number defined by the following process:
+// Starting with any positive integer, replace the number by the sum of the squares of its digits.Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+// Those numbers for which this process ends in 1 are happy.
+// Return true if n is a happy number, and false if not.
+
+//Example 1:
+// Input: n = 19
+// Output: true
+
+// Example 2:
+// Input: n = 2
+// Output: false
 func HappyNumberPrac(num int) bool {
 	return false
 }
