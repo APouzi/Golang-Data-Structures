a227ffab5e8841fb6f369eab740ac8bfebd8ed30
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-14 17:19:48 +0900

    added merging of intervals practice -AP

--- Patterns/PatternPractice.go
+++ Patterns/PatternPractice.go
@@ -11,3 +11,14 @@ func cycleHN(num int) int {
 func PermuStrPrac(str1 string, str2 string) bool {
 	return false
 }
+
+func MergeIntervalsPrac(intervals [][]int) [][]int {
+	return [][]int{}
+}
+
+func MaxPrac(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
