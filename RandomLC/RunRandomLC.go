a58c365a475d2eac0761a99af67357b267de0d6d
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-14 17:49:58 +0900

    added start of k group maximum subarray -AP

--- RandomLC/RunRandomLC.go
+++ RandomLC/RunRandomLC.go
@@ -20,3 +20,13 @@ func ShuffleString(s string, indices []int)string{
 	var newSR string = string(newS)
 	return newSR
 }
+
+//You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+// Return the max sliding window.
+//Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+// Output: [3,3,5,5,6,7]
+
+func maxSlidingWindow(arr []int, k int)[]int{
+	return []int{}
+}
