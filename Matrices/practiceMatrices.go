a5d8c22d54ae09e44b8d7bef866927f5d71809e6
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-19 02:17:16 +0900

    updated the practice matrix stuff -AP

---
+++ Matrices/practiceMatrices.go
@@ -0,0 +1,44 @@
+package matrices
+
+// {1, 2, 3, 4},
+// {12, 13, 14, 5},
+// {11, 16, 15, 6},
+// {10, 9, 8, 7},
+func SpiralMatrixPrac(arr [][]int) []int {
+
+	return []int{}
+}
+
+//rotate image 90 degrees
+// {1, 2, 3,},
+// {12, 13, 5},
+// {10, 9, 7},
+
+func RotateMatrixPrac(arr [][]int)[][]int{
+	return arr
+}
+
+//if there is a zero, turn all of the values in it's row and column into 0s
+func SetMatrixZerosPrac(arr [][]int)[][]int{
+	
+
+	return arr
+
+}
+
+//Mirror the matrix array
+func MirrorMatrixPrac(arr [][]int)[][]int{
+	return [][]int{}
+}
+
+// [1,1,1,-1,-1],
+// [1,1,1,-1,-1],
+// [-1,-1,-1,1,1],
+// [1,1,1,1,-1],
+// [-1,-1,-1,-1,-1]]
+//Given values of a that represent a left leaning wall (-1) and right leaning wall (1), where will the ball drop, even if it can?
+func WhereWillTheBallDropPrac(arr [][]int)[]int{
+	return []int{}
+}
+
+
