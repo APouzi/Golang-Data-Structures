fc92c4ba8c93b775400ad40e8fbbb37de115e14d
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-20 05:10:32 +0900

    added fruits in basket sliding window problem -AP

--- Patterns/SlidingWindow.go
+++ Patterns/SlidingWindow.go
@@ -106,3 +106,47 @@ func NumSubarrayProductLessThanK(nums []int, k int) int {
     }
     return ans
 }
+
+
+// You are visiting a farm that has a single row of fruit trees arranged from left to right. The trees are represented by an integer array fruits where fruits[i] is the type of fruit the ith tree produces.You want to collect as much fruit as possible. However, the owner has some strict rules that you must follow:
+
+// You only have two baskets, and each basket can only hold a single type of fruit. There is no limit on the amount of fruit each basket can hold.
+// Starting from any tree of your choice, you must pick exactly one fruit from every tree (including the start tree) while moving to the right. The picked fruits must fit in one of your baskets.
+// Once you reach a tree with fruit that cannot fit in your baskets, you must stop.
+// Given the integer array fruits, return the maximum number of fruits you can pick.
+
+// Example 1:
+// Input: fruits = [1,2,1]
+// Output: 3
+// Explanation: We can pick from all 3 trees.
+
+// Example 2:
+// Input: fruits = [0,1,2,2]
+// Output: 3
+// Explanation: We can pick from trees [1,2,2].
+// If we had started at the first tree, we would only pick from trees [0,1].
+
+// Example 3:
+// Input: fruits = [1,2,3,2,2]
+// Output: 4
+// Explanation: We can pick from trees [2,3,2,2].
+// If we had started at the first tree, we would only pick from trees [1,2].
+
+func TotalFruit(fruits []int) int {
+    var bank map[int]int = make(map[int]int)
+    var a int = 0
+    var ans int = 0
+    for i:=0;i<len(fruits);i++{
+        bank[fruits[i]]+= 1
+        for len(bank)>2{
+            bank[fruits[a]]-=1
+            if bank[fruits[a]] == 0{
+                delete(bank, fruits[a])
+            }
+            a++
+        }
+        ans = max(ans, i-a+1)
+    }
+    return ans
+    
+}
