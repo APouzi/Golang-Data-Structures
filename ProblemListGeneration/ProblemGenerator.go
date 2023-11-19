60afad42d5c2a2eb2b56dbdfe5aa2e10f002a796
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-20 05:10:56 +0900

    added a bunch of problem generator stuff -AP

--- ProblemListGeneration/ProblemGenerator.go
+++ ProblemListGeneration/ProblemGenerator.go
@@ -7,7 +7,7 @@ import (
 var AllProblems []string = []string{
 	"mirror matrix", "rotate matrix", "spiral matrix", "where will the ball drop", "set matrix zeros", 
 	"Linked list", "Cycle Detection","middle of list", " reverse linked list iterative", "reverse k groups", 
-	"reverse linked list recursive", "delete duplicates linked list", "delete specific node","create a binary heap",
+	"reverse linked list recursive", "delete duplicates linked list", "delete specific LL node","create a binary heap",
 	"merge intervals","insert interval","remove minimum over lapping interval","happy number", "valid parenthesis",
 	"DFS a Graph","BFS a Graph", "Union Find a Graph", "Number of Islands","Class Schedule", "valid palindrome", 
 	"valid anagram", "Sort a Linked List", "Quick Sort", "Merge Sort", "Binary Search", "Permutation of a String", 
@@ -15,7 +15,10 @@ var AllProblems []string = []string{
 	"Right View of Tree", "Sum Path BST", "Validate BST", "In Order, Post Order, Pre Order Traversal of a Tree",
 	"Check Sum BST", "Buy High Sell Low","Contains Duplicate", "Kth Smallest Element in a BST", "All Paths with Sum to 'k'",
 	"Merge K Sorted Lists", "Longest Substring without repeating Characters", "Implement a Trie", "Fruits in a Basket",
-	"Count all substring that equal to 'k", "Implement a Trie", "Keys and Rooms (Topological Sorting)", "Sum of Nodes with Even-Valued Grandparent", "Course Schedule II"}
+	"Count all substring that equal to 'k", "Implement a Trie", "Keys and Rooms (Topological Sorting)", 
+	"Sum of Nodes with Even-Valued Grandparent", "Course Schedule II", "Remove Nth Node From a List",
+	"SubArray Product less than K","Merge K Sorted Lists","Merge 2 Sorted Lists", "Intersection of two Linked Lists",
+	 "Maximum Subarray", "Delete Node from a BST", "Two Sum", "Path Sum II", "Invert a Binary Tree"}
 func GenerationProblemOne() {
 	fmt.Println("\nTotal Problems in Knowledge Bank is:",len(AllProblems))
 	fmt.Println("\n",AllProblems[rand.Intn(len(AllProblems))])
