7a46f6b7fc618fbb779c7465c33f1311ff7680fd
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-20 05:04:29 +0900

    added invert of binary tree -AP

---
+++ Trees/InvertBinaryTree.go
@@ -0,0 +1,15 @@
+package bst
+
+// Given the root of a binary tree, invert the tree, and return its root
+
+//Ex2
+// Input: root = [4,2,7,1,3,6,9]
+// Output: [4,7,2,9,6,3,1]
+
+//Ex2
+// Input: root = [2,1,3]
+// Output: [2,3,1]
+
+func InvertTree(root *Node) *Node {
+    return root
+}
