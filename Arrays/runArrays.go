f825c1c47349872a96fece1c25f499f768fc2610
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-19 20:54:29 +0900

    added run arrays problems with practice divider -AP

--- Arrays/runArrays.go
+++ Arrays/runArrays.go
@@ -1,1 +1,12 @@
 package arrays
+
+import "fmt"
+
+func RunArrays() {
+	fmt.Println("Array Problems Have Begun")
+	fmt.Println("Shuffle String", ShuffleString("codeleet", []int{4,5,6,7,0,2,1,3}))
+
+	//Practice Problems
+	fmt.Println("Practice Array Problems")
+	fmt.Println("Shuffle String Practice", ShuffleString("codeleet", []int{4,5,6,7,0,2,1,3}))
+}
