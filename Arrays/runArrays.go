8b75bec21d68a933d548c084c57ce9384831900e
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-19 21:24:14 +0900

    added some notes and formatting for arrays -AP

--- Arrays/runArrays.go
+++ Arrays/runArrays.go
@@ -3,10 +3,10 @@ package arrays
 import "fmt"
 
 func RunArrays() {
-	fmt.Println("Array Problems Have Begun")
-	fmt.Println("Shuffle String", ShuffleString("codeleet", []int{4,5,6,7,0,2,1,3}))
+	fmt.Println("---Array Problems Have Begun---")
+	fmt.Println("Shuffle String:", ShuffleString("codeleet", []int{4,5,6,7,0,2,1,3}))
 
 	//Practice Problems
-	fmt.Println("Practice Array Problems")
-	fmt.Println("Shuffle String Practice", ShuffleString("codeleet", []int{4,5,6,7,0,2,1,3}))
+	fmt.Println("----Practice Array Problems---")
+	fmt.Println("Shuffle String Practice:", ShuffleString("codeleet", []int{4,5,6,7,0,2,1,3}))
 }
