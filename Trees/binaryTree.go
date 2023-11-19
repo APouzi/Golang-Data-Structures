045af55179a7ea3aa5360ee32089e7d61c8212a9
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-19 19:40:21 +0900

    started the removal of a bunch of algorithms into their own files -AP

--- Trees/binaryTree.go
+++ Trees/binaryTree.go
@@ -91,27 +91,7 @@ func insertRecur(node *Node, num int) *Node {
 	return node
 }
 
-func BFSonBST(node *Node) {
-	queue := []*Node{}
-	queue = append(queue, node)
-	path := []int{}
 
-	for len(queue) > 0 {
-		popped := queue[0]
-		queue = queue[1:]
-		if popped.left != nil {
-			queue = append(queue, popped.left)
-		}
-
-		if popped.right != nil {
-			queue = append(queue, popped.right)
-		}
-
-		path = append(path, popped.val)
-
-	}
-	fmt.Print(path)
-}
 
 func traversal(node *Node) {
 	fmt.Println(node.val)
@@ -124,57 +104,10 @@ func traversal(node *Node) {
 	}
 }
 
-func rightView(node *Node) {
-	queue := []*Node{}
-	queue = append(queue, node)
-	path := []int{}
 
-	for len(queue) > 0 {
-		rightSide := -1
-		currLength := len(queue)
-		for i := 0; i < currLength; i++ {
-			popped := queue[0]
-			queue = queue[1:]
 
-			if popped.left != nil {
-				queue = append(queue, popped.left)
-			}
 
-			if popped.right != nil {
-				queue = append(queue, popped.right)
-			}
-			rightSide = popped.val
-		}
 
-		path = append(path, rightSide)
-
-	}
-	fmt.Print(path)
-}
-
-func longestDiameter(node *Node) int {
-	if node == nil {
-		return 0
-	}
-
-	left := longestDiameter(node.left)
-	right := longestDiameter(node.right)
-
-	return max(left, right) + 1
-}
-
-func bstLongest(node *Node, ans *int) int {
-	if node == nil {
-		return 0
-	}
-
-	left := bstLongest(node.left, ans)
-	right := bstLongest(node.right, ans)
-	left = max(left, 0)
-	right = max(right, 0)
-	*ans = max(*ans, left+right+node.val)
-	return max(left, right) + node.val
-}
 
 func max(a int, b int) int {
 	if a > b {
@@ -184,106 +117,15 @@ func max(a int, b int) int {
 	}
 }
 
-func sumPath(node *Node, sum int, temp int) bool {
-	if node == nil {
-		return false
-	}
-	if node.right == nil && node.left == nil && temp+node.val == sum {
-		return true
-	}
-	fmt.Println(node.val)
-	left := sumPath(node.left, sum, temp+node.val)
-	right := sumPath(node.right, sum, temp+node.val)
 
-	return left || right
-}
 
-func biggestPath(node *Node, sums *[]int, temp int) bool {
-	if node == nil {
-		return true
-	}
-	if node.right == nil && node.left == nil {
-		*sums = append(*sums, temp+node.val)
-		fmt.Println(sums)
-		return true
-	}
-	//
-	biggestPath(node.left, sums, temp+node.val)
-	biggestPath(node.right, sums, temp+node.val)
-	return true
-}
 
-func checkSumRecur(node *Node, sum int, temp int, listTemp []int, list [][]int) bool {
 
-	if node == nil {
-		return false
-	}
-	listTemp = append(listTemp, node.val)
 
-	if node.left == nil && node.right == nil && temp+node.val == sum {
 
-		list = append(list, listTemp)
-		fmt.Println("reached:", list)
-		return true
-	}
-	checkSumRecur(node.left, sum, temp+node.val, listTemp, list)
-	checkSumRecur(node.right, sum, temp+node.val, listTemp, list)
-
-	return true
-}
-
-func validateTreeInit(node *Node) bool {
-	min := -9999999
-	max := 9999999
-
-	return validateTree(node, min, max)
-}
-
-func validateTree(node *Node, min int, max int) bool {
-	if node == nil {
-		return true
-	}
-
-	left := validateTree(node.left, min, node.val)
-	right := validateTree(node.right, node.val, max)
-	if node.val >= max || node.val <= min {
-		return false
-	}
-	return left && right
-}
 
 func rightSide(node *Node, min int, max int) bool {
 	return true
 }
 
 
-func InOrderTraversal(node *Node){
-	if node.left != nil{
-		InOrderTraversal(node.left)
-	}
-	fmt.Println(node.val)
-	if node.right != nil {
-		InOrderTraversal(node.right)
-	}
-
-}
-
-func PostOrderTraversal(node *Node){
-	if node.right != nil{
-		PostOrderTraversal(node)
-	}
-	if node.left != nil{
-		PostOrderTraversal(node)
-	}
-	fmt.Println(node.val)
-}
-
-func PreOrderTraversal(node *Node){
-	fmt.Println(node.val)
-	if node.right != nil{
-		PostOrderTraversal(node)
-	}
-	if node.left != nil{
-		PostOrderTraversal(node)
-	}
-}
