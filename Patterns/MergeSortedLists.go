af095d22a6730472789479283729781bfbf7d3df
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-20 04:34:17 +0900

    added merge 2 sorted lists -AP

--- Patterns/MergeSortedLists.go
+++ Patterns/MergeSortedLists.go
@@ -47,3 +47,52 @@ package patterns
 // 	mergedTail.Next = nil
 // 	return dummyHead.Next
 // }
+
+
+
+
+
+
+// You are given the heads of two sorted linked lists list1 and list2.
+// Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+// Return the head of the merged linked list.
+
+// Example 1:
+// Input: list1 = [1,2,4], list2 = [1,3,4]
+// Output: [1,1,2,3,4,4]
+
+// Example 2:
+// Input: list1 = [], list2 = []
+// Output: []
+
+// Example 3:
+// Input: list1 = [], list2 = [0]
+// Output: [0]
+
+// func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+
+//     if list1 == nil || list2 == nil {
+//         if list1 == nil{
+//             return list2
+//         }
+//         if list2 == nil{
+//             return list1
+//         }
+
+//     }
+
+//     minHeap := MinHeap{}
+//     minHeap.push(list1)
+//     minHeap.push(list2)
+//     dummyHead := &ListNode{Val:-111}
+//     mergedTail := dummyHead
+//     for len(minHeap.array) > 0{
+//         popped := minHeap.pop()
+//         mergedTail.Next = popped
+//         mergedTail = popped
+//         if popped.Next != nil{
+//             minHeap.push(popped.Next)
+//         }
+//     }
+//     return dummyHead.Next
+// }
