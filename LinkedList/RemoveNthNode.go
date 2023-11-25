1a565db1346b9821801dba5b0384bf4a9a20cec2
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-26 02:39:35 +0900

    RemoveNthNode has been added to list -AP

--- LinkedList/RemoveNthNode.go
+++ LinkedList/RemoveNthNode.go
@@ -1,2 +1,17 @@
 package LinkedList
 
+func RemoveNthFromEnd(head *LinkedNode, n int) *LinkedNode {
+	var dummyHead *LinkedNode = &LinkedNode{}
+	dummyHead.Next = head
+	var forward, prevBeforeRemoval *LinkedNode = dummyHead, dummyHead
+	for forward.Next != nil {
+		if n <= 0 {
+			prevBeforeRemoval = prevBeforeRemoval.Next
+		}
+		forward = forward.Next
+		n--
+	}
+	var theNodeToDelete *LinkedNode = prevBeforeRemoval.Next
+	prevBeforeRemoval.Next = theNodeToDelete.Next
+	return dummyHead.Next
+}
