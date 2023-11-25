427ebaf235ef0cf8bcd14e2159d0a00f374371a1
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-26 06:41:39 +0900

    this is going ot be the findDuplicate number, it requires special input array for this to work properly -AP

--- Patterns/FastAndSlowPointers.go
+++ Patterns/FastAndSlowPointers.go
@@ -53,3 +53,31 @@ func CycleDetect(node *LinkedNode) bool {
 	}
 	return false
 }
+
+//Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+// There is only one repeated number in nums, return this repeated number.
+// You must solve the problem without modifying the array nums and uses only constant extra space.
+
+// Example 1:
+// Input: nums = [1,3,4,2,2]
+// Output: 2
+
+// Example 2:
+// Input: nums = [3,1,3,4,2]
+// Output: 3
+
+func FindDuplicate(nums []int) int {
+    var slow, fast int = nums[0], nums[nums[0]]
+
+    for slow != fast{
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+    }
+    fast = 0
+    for slow != fast{
+        slow = nums[slow]
+        fast = nums[fast]
+    }
+    return fast
+}
