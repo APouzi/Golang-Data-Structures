d04e5b9b90b914cc031119457bc6ad67f1ad829b
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-11-27 01:52:37 +0900

    added valid anagram, hash map version -AP

--- Arrays/ValidAnagram.go
+++ Arrays/ValidAnagram.go
@@ -1,3 +1,22 @@
 package arrays
 
+func IsAnagramHashMap(s string, t string) bool {
+	var tl, sl int = len(s), len(t)
+	if sl != tl {
+		return false
+	}
+	var sm, tm map[byte]int = make(map[byte]int, tl), make(map[byte]int, sl)
+	for i := 0; i < tl; i++ {
+		sm[s[i]]++
+		tm[t[i]]++
+	}
+
+	for i := 0; i < tl; i++ {
+		if sm[s[i]] != tm[s[i]] {
+			return false
+		}
+	}
+	return true
+}
+
 
