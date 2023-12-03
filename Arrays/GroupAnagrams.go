9c707640de763a08f7c6648cee8c1bceb4012cd1
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-12-03 21:12:12 +0900

    this is going to be group anagrams -AP

--- Arrays/GroupAnagrams.go
+++ Arrays/GroupAnagrams.go
@@ -1,5 +1,20 @@
 package arrays
 
+//Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+// Example 1:
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+// Example 2:
+// Input: strs = [""]
+// Output: [[""]]
+
+// Example 3:
+// Input: strs = ["a"]
+// Output: [["a"]]
+
 func GroupAnagrams(strs []string) [][]string {
 	var groups map[[26]int][]string = map[[26]int][]string{}
 	for i := 0; i < len(strs); i++ {
