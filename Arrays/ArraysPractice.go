e7fdbce31fecc0f14e93a6a5ab03056595d4a2da
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-12-04 06:34:31 +0900

    updated practice arrays for future ease of practice -AP

--- Arrays/ArraysPractice.go
+++ Arrays/ArraysPractice.go
@@ -130,11 +130,26 @@ func IsAnamgramPrac(s string, t string) bool {
 	return false
 }
 
-// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
-// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+//Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
 
-// Example 1:
-// Input: s = "anagram", t = "nagaram"
+// Each row must contain the digits 1-9 without repetition.
+// Each column must contain the digits 1-9 without repetition.
+// Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+// Note:
+
+// A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+// Only the filled cells need to be validated according to the mentioned rules
+
+//Input: board = 
+// [["5","3",".",".","7",".",".",".","."]
+// ,["6",".",".","1","9","5",".",".","."]
+// ,[".","9","8",".",".",".",".","6","."]
+// ,["8",".",".",".","6",".",".",".","3"]
+// ,["4",".",".","8",".","3",".",".","1"]
+// ,["7",".",".",".","2",".",".",".","6"]
+// ,[".","6",".",".",".",".","2","8","."]
+// ,[".",".",".","4","1","9",".",".","5"]
+// ,[".",".",".",".","8",".",".","7","9"]]
 // Output: true
 
 // Example 2:
