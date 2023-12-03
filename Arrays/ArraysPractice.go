0842abb19b9a978d47d789d113020bf1337f4b19
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-12-03 21:10:01 +0900

    new arrays practice file with description -AP

---
+++ Arrays/ArraysPractice.go
@@ -0,0 +1,134 @@
+package arrays
+
+//Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+// Example 1:
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+// Example 2:
+// Input: strs = [""]
+// Output: [[""]]
+
+// Example 3:
+// Input: strs = ["a"]
+// Output: [["a"]]
+func GroupAnagramsPract(strs []string) [][]string {
+	return [][]string{}
+}
+
+func FindKthLargestPrac(nums []int, k int) int {
+	return -1
+}
+
+//Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+// You must write an algorithm that runs in O(n) time.
+
+// Example 1:
+// Input: nums = [100,4,200,1,3,2]
+// Output: 4
+// Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+
+// Example 2:
+// Input: nums = [0,3,7,2,5,8,4,6,0,1]
+// Output: 9
+func LongestConsercutiveSequence(nums []int) int {
+	return -1
+}
+
+//Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+// Example 1:
+// Input: nums = [3,0,1]
+// Output: 2
+// Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+// Example 2:
+// Input: nums = [0,1]
+// Output: 2
+// Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+
+// Example 3:
+// Input: nums = [9,6,4,2,3,5,7,0,1]
+// Output: 8
+// Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+
+func MissingNumberPrac(nums []int) int {
+	return -1
+}
+
+//Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i]. The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+// You must write an algorithm that runs in O(n) time and without using the division operation.
+
+// Example 1:
+// Input: nums = [1,2,3,4]
+// Output: [24,12,8,6]
+
+// Example 2:
+// Input: nums = [-1,1,0,-3,3]
+// Output: [0,0,9,0,0]
+
+func ProductExceptSelfPrac(nums []int) []int {
+	return []int{}
+}
+
+// Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+// Example 1:
+// Input: nums = [1,1,1,2,2,3], k = 2
+// Output: [1,2]
+
+// Example 2:
+// Input: nums = [1], k = 1
+// Output: [1]
+
+//nums =[5,3,1,1,1,3,73,1]
+// k = 2
+
+func TopKFrequentPrac(nums []int, k int) []int {
+	return []int{}
+}
+
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+func TwoSumPrac() {
+
+}
+
+func IsAnamgramPrac(s string, t string) bool {
+	return false
+}
+
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+// Example 1:
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+
+// Example 2:
+// Input: s = "rat", t = "car"
+// Output: false
+
+func ValidSudokuPrac(s, t string) bool {
+	return false
+}
