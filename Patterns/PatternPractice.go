f43cc568bde4f0dd65ed388102769c1790ec54d2
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-12-05 23:30:35 +0900

    Added descriptions and practice problems based off the sliding window -AP

--- Patterns/PatternPractice.go
+++ Patterns/PatternPractice.go
@@ -68,3 +68,149 @@ func FindDuplicatePrac([]int)int{
 func GetSkylinePrac(buildings [][]int)[][]int{
 	return buildings
 }
+
+
+// Given a string(str1), is str2 a permutation of str1?
+// Example str1 = "abcdefg", str2 = "bac". "bac" is a permuatation of "abcdefg" because "abc" of "abcdefg" can be permutated to "bac"
+// test cases:
+// str1 = "eidbaooo", str2 = "ab"
+// str1 = "eidboaoo", str2 = "ab"
+// str1 = "eidboaoo", str2 "eidboaoo"
+
+func PermutationOfString(str1, str2 string) bool{
+	return false
+}
+
+
+// Given an integer array nums, find a
+// subarray
+//  that has the largest product, and return the product.
+// The test cases are generated so that the answer will fit in a 32-bit integer.
+
+// Example 1:
+// Input: nums = [2,3,-2,4]
+// Output: 6
+// Explanation: [2,3] has the largest product 6.
+
+// Example 2:
+// Input: nums = [-2,0,-1]
+// Output: 0
+// Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+func MaxProductPrac(nums []int) int {
+	return -1
+}
+
+//Given an array of integers nums and an integer k, return the number of contiguous subarrays where the product of all the elements in the subarray is strictly less than k.
+
+// Example 1:
+// Input: nums = [10,5,2,6], k = 100
+// Output: 8
+// Explanation: The 8 subarrays that have product less than 100 are:
+// [10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6]
+// Note that [10, 5, 2] is not included as the product of 100 is not strictly less than k.
+
+// Example 2:
+// Input: nums = [1,2,3], k = 0
+// Output: 0
+
+func NumSubarrayProductLessThanKPrac(nums []int, k int) int {
+	return -1
+}
+
+//Given a string s, find the length of the longest substring without repeating characters.
+
+// Example 1:
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+
+// Example 2:
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+
+// Example 3:
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+//testcases: "qrsvbspk" = 5, " " = 1
+//first one will fail without a for loop inside deleting instead of if loop.
+
+func LengthOfLongestSubstringPrac(s string) int {
+	return -1
+}
+
+// You are visiting a farm that has a single row of fruit trees arranged from left to right. The trees are represented by an integer array fruits where fruits[i] is the type of fruit the ith tree produces.You want to collect as much fruit as possible. However, the owner has some strict rules that you must follow:
+
+// You only have two baskets, and each basket can only hold a single type of fruit. There is no limit on the amount of fruit each basket can hold.
+// Starting from any tree of your choice, you must pick exactly one fruit from every tree (including the start tree) while moving to the right. The picked fruits must fit in one of your baskets.
+// Once you reach a tree with fruit that cannot fit in your baskets, you must stop.
+// Given the integer array fruits, return the maximum number of fruits you can pick.
+
+// Example 1:
+// Input: fruits = [1,2,1]
+// Output: 3
+// Explanation: We can pick from all 3 trees.
+
+// Example 2:
+// Input: fruits = [0,1,2,2]
+// Output: 3
+// Explanation: We can pick from trees [1,2,2].
+// If we had started at the first tree, we would only pick from trees [0,1].
+
+// Example 3:
+// Input: fruits = [1,2,3,2,2]
+// Output: 4
+// Explanation: We can pick from trees [2,3,2,2].
+// If we had started at the first tree, we would only pick from trees [1,2].
+
+func TotalFruitPrac(fruits []int) int {
+	return -1
+}
+
+// You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
+// Return the length of the longest substring containing the same letter you can get after performing the above operations.
+
+// Example 1:
+// Input: s = "ABAB", k = 2
+// Output: 4
+// Explanation: Replace the two 'A's with two 'B's or vice versa.
+
+// Example 2:
+// Input: s = "AABABBA", k = 1
+// Output: 4
+// Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+// The substring "BBBB" has the longest repeating letters, which is 4.
+// There may exists other ways to achieve this answer too.
+
+func CharacterReplacementPrac(s string, k int) int {
+	return -1
+}
+
+
+// Given two strings s and t of lengths m and n respectively, return the minimum window
+// substring of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+// The testcases will be generated such that the answer is unique.
+
+// Example 1:
+// Input: s = "ADOBECODEBANC", t = "ABC"
+// Output: "BANC"
+// Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+
+// Example 2:
+// Input: s = "a", t = "a"
+// Output: "a"
+// Explanation: The entire string s is the minimum window.
+
+// Example 3:
+// Input: s = "a", t = "aa"
+// Output: ""
+// Explanation: Both 'a's from t must be included in the window.
+// Since the largest window of s only has one 'a', return empty string.
+
+
+func MinWindow(s string, t string) string {
+	return "practice"
+}
--- Patterns/SlidingWindow/MinimumWindowSubstring.go
+++ Patterns/SlidingWindow/MinimumWindowSubstring.go
@@ -1,5 +1,26 @@
 package slidingwindow
 
+// Given two strings s and t of lengths m and n respectively, return the minimum window
+// substring of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+// The testcases will be generated such that the answer is unique.
+
+// Example 1:
+// Input: s = "ADOBECODEBANC", t = "ABC"
+// Output: "BANC"
+// Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+
+// Example 2:
+// Input: s = "a", t = "a"
+// Output: "a"
+// Explanation: The entire string s is the minimum window.
+
+// Example 3:
+// Input: s = "a", t = "aa"
+// Output: ""
+// Explanation: Both 'a's from t must be included in the window.
+// Since the largest window of s only has one 'a', return empty string.
+
+
 func MinWindow(s string, t string) string {
 	var compareTo map[byte]int = make(map[byte]int)
 	var window map[byte]int = make(map[byte]int)
