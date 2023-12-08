7dfb0ef8f5f79a6c967454a8415d365d7e24ce3a
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-12-09 06:55:50 +0900

    added the monotonic algorithim that allows us to calculate what the result of the max sliding window is going to be -AP

--- Patterns/SlidingWindow/MaxSlidingWindow.go
+++ Patterns/SlidingWindow/MaxSlidingWindow.go
@@ -4,7 +4,57 @@ import (
 	"container/heap"
 )
 
-//My attempt but way too slow:
+//You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+// Return the max sliding window.
+
+//Example 1:
+// Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+// Output: [3,3,5,5,6,7]
+// Explanation:
+// Window position                Max
+// ---------------               -----
+// [1  3  -1] -3  5  3  6  7       3
+//  1 [3  -1  -3] 5  3  6  7       3
+//  1  3 [-1  -3  5] 3  6  7       5
+//  1  3  -1 [-3  5  3] 6  7       5
+//  1  3  -1  -3 [5  3  6] 7       6
+//  1  3  -1  -3  5 [3  6  7]      7
+
+// Example 2:
+// Input: nums = [1], k = 1
+// Output: [1]
+
+
+
+func MaxSlidingWindow(nums []int, k int)[]int{
+	var mono, ans []int = make([]int, 0), make([]int, 0)
+
+	var l int = 0
+	for r := 0; r < len(nums);r++{
+		for len(mono) > 0 && nums[mono[len(mono)-1]] < nums[r]{
+			mono = mono[:len(mono)-1]
+		}
+		mono = append(mono, r)
+
+		if mono[0] < l{
+			mono = mono[1:]
+		} 
+
+		if (r+1) >= k{
+			ans = append(ans, nums[mono[0]])
+			l++
+		}
+
+	}
+	return ans
+}
+
+
+
+
+
+//My attempt but way too slow, especially on large sets of data:
 type MaxHeap []int
 
 func(h MaxHeap) Len()int{ return len(h)}
