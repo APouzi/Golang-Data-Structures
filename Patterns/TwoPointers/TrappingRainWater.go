56b77f286a8089381908c8dab501f9732f939b00
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-12-21 11:22:05 +0100

    Trapping rain water has been updated in it's explanation -AP

--- Patterns/TwoPointers/TrappingRainWater.go
+++ Patterns/TwoPointers/TrappingRainWater.go
@@ -19,12 +19,15 @@ func TrappingRainWater(height []int) int {
 	var leftMax, rightMax int = height[left], height[right]
 
 	for left < right {
-		if leftMax < rightMax {
+		//[0,1,0,2,1,0,1,3,2,1,2,1]
+		//>>>>>>>> 		 <<<<<<<<<<
+		//We are getting max left and right, and when we check the trapped water, we are only checking the one that is ahead of the max left and right. NOT the one behind it.
+		if leftMax < rightMax {//This is asking which boundary is the smallest out of the two. We ONLY want the smallest of the two as that is what matters in retaining water.
 			left++
-			leftMax = max(height[left], leftMax) // As we go forward, we need to be asking which "wall" is the hieghest. This is because with this answer, we are able to ascertain the ith biggest to the right/left. With that knowledge, we are able to get the smallest 
+			leftMax = max(height[left], leftMax) // As we go forward, we need to be asking which "wall" is the heighest. This is because with this answer, we are able to ascertain the ith biggest to the left. With that knowledge, we are able to get the biggest wall. This answer will only change when we find the next biggest wall. What is import is that we are only doing calculations based on the walls that are lower or equal to it to get the water that each isolated unit can hold.
 
-			ans += leftMax - height[left] //These will will never be negative, the reason why is because leftMax is only able to reach on the same value as height[left] and never less, this is because leftMax can only be bigger or the same value as height[left] which would amount to 0. leftMax can never be "ahead" of left, just in parallel. Same applies to right max.
-		} else {
+			ans += leftMax - height[left] //These will will never be negative, the reason why is because leftMax is only able to reach on the same value as height[left] when it updates and never less. leftMax can only be bigger or the same value as height[left] which would amount to 0. leftMax can never be "ahead" of left, just at the same level. Same applies to rightMax.
+		} else { 
 			right--
 			rightMax = max(height[right], rightMax)
 			ans += rightMax - height[right]
@@ -32,3 +35,18 @@ func TrappingRainWater(height []int) int {
 	}
 	return ans
 }
+
+//-------The Why requires understanding another algorithm-------
+
+//The above algorithm is the replacement of the algorithmm below:
+
+//LeftMax: One loop starts from the left or 0th index and saves the state of the maxima at each iteration
+//RightMax: One loop starts from the right or last index and goes to the 0th index and saves the state of the maxima at each iteration
+//MinLR: The third loop will get the min of the LeftMax and RightMax and store at each iteration.
+//Lastly, we loop over the original list of heights and subtract the MinLR from heights[i]. Then increment that result.
+//LeftMax:	  [0,1,1,2,2,2,2,3,3,3,3,3]
+//RightMax:	  [1,2,2,2,3,3,3,3,3,3,3,3]
+//MinLR:   	  [0,1,1,2,2,2,2,3,3,3,3,3]
+//Last Loop:  ans += heights[i] - MinLR[i] 
+
+//With our version, we are replacing the need for four loops and instead, only moving up when the leftMax/rightMax is smaller than it's counterpart. That way we achieve the same exact thing as leftMax and rightMax loops, but only doing when one is smaller than the other. 
