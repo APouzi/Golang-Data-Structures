1105369309d7c6d68c8f8d03e962c5952aa1ef27
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-12-21 18:51:22 +0100

    Merge Intervals Prac -AP

--- Patterns/PatternPractice.go
+++ Patterns/PatternPractice.go
@@ -21,6 +21,19 @@ func cycleHN(num int) int {
 	return -1
 }
 
+//Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+// Example 1:
+// Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+// Output: [[1,6],[8,10],[15,18]]
+// Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+
+// Example 2:
+// Input: intervals = [[1,4],[4,5]]
+// Output: [[1,5]]
+// Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+
+//Note, DO NOT use quicksort for intervals, this is because quicksort intervals is unstable.
 func MergeIntervalsPrac(intervals [][]int) [][]int {
 	return [][]int{}
 }
