46af0605381f76ff016ee74e86df3b97ffef0b27
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-12-21 11:31:45 +0100

    forgot to add this one -AP

--- Patterns/PatternPractice.go
+++ Patterns/PatternPractice.go
@@ -344,6 +344,6 @@ func TrappingRainWater(height []int) int {
 // Input: numbers = [-1,0], target = -1
 // Output: [1,2]
 // Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
-func TwoSumII(numbers []int, target int) []int {
+func TwoSumIIPrac(numbers []int, target int) []int {
 	return []int{}	
 }
