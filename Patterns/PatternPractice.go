b8a20faab4f69ad16018bbec8a3d5c4abc367974
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-12-21 20:50:09 +0100

    non over lapping intervals practice description -AP

--- Patterns/PatternPractice.go
+++ Patterns/PatternPractice.go
@@ -43,6 +43,23 @@ func InsertIntervalsPrac(intervals [][]int, newInterval []int) [][]int {
 	return [][]int{}
 }
 
+
+//Given an array of intervals intervals where intervals[i] = [starti, endi], return the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
+// Example 1:
+// Input: intervals = [[1,2],[2,3],[3,4],[1,3]]
+// Output: 1
+// Explanation: [1,3] can be removed and the rest of the intervals are non-overlapping.
+
+// Example 2:
+// Input: intervals = [[1,2],[1,2],[1,2]]
+// Output: 2
+// Explanation: You need to remove two [1,2] to make the rest of the intervals non-overlapping.
+
+// Example 3:
+// Input: intervals = [[1,2],[2,3]]
+// Output: 0
+// Explanation: You don't need to remove any of the intervals since they're already non-overlapping.
+//NOTE as example 3 shows, if two edges are the same, they are NOT overlapping.
 // Input: intervals = [[1,2],[2,3],[3,4],[1,3]] Output: 1 [[1,2],[1,3],[2,3],[3,4]]
 // Input: intervals = [[1,2],[1,2],[1,2]] Output: 2
 func nonOverLappingIntervalsPrac(intervals [][]int) int {
