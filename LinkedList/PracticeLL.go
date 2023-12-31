f2b12f967d293b867c3bcf6199de4c484ce634a2
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-12-31 17:38:50 +0100

    updated comments for practice -AP

--- LinkedList/PracticeLL.go
+++ LinkedList/PracticeLL.go
@@ -1,7 +1,7 @@
 package LinkedList
 
 // The List passed in: 9, 8, 7, 6, 5, 4, 3, 2, 1
-// the result: 7,8, 9, 6, 5, 4, 3, 2, 1
+// the result: 7, 8, 9, 4, 5, 6, 1, 2, 3
 func ReverseKGroupsPrac(head *LinkedNode, k int) *LinkedNode {
 	return head
 }
