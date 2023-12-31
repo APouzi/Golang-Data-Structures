aabc7b84a0032c46c3f4599fa0d7fae859c13434
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-12-31 17:57:34 +0100

    added some extra problems to run while cleaningup 2d Array, also added utils -AP

--- Arrays/ArrayUtils.go
+++ Arrays/ArrayUtils.go
@@ -73,3 +73,77 @@ func (h *Heap) leftChild(index int) int {
 func (h *Heap) rightChild(index int) int {
 	return (index * 2) + 2
 }
+
+type MinHeapPrac struct {
+	arr []int
+}
+
+func InitializeMinHeapPrac() *MinHeapPrac {
+	return &MinHeapPrac{arr: []int{}}
+}
+
+func (h *MinHeapPrac) Pop() int {
+	popped := h.arr[0]
+	var lastIndex int = len(h.arr) - 1
+	h.arr[0] = h.arr[lastIndex]
+	h.arr = h.arr[:lastIndex]
+	h.HeapifyDown(0)
+	return popped
+}
+
+func (h *MinHeapPrac) Insert(num int) {
+	h.arr = append(h.arr, num)
+	var lastIndex int = len(h.arr) - 1
+	h.HeapifyUp(lastIndex)
+}
+
+func (h *MinHeapPrac) HeapifyUp(index int) {
+	for h.arr[index] < h.arr[h.Parent(index)] {
+		h.arr[index], h.arr[h.Parent(index)] = h.arr[h.Parent(index)], h.arr[index]
+		index = h.Parent(index)
+	}
+}
+
+func (h *MinHeapPrac) HeapifyDown(index int) {
+	var lastIndex int = len(h.arr) - 1
+	var biggestChild int
+	for h.leftChild(index) <= lastIndex {
+		biggestChild = h.leftChild(index)
+		if h.rightChild(index) <= lastIndex {
+			if h.arr[biggestChild] > h.arr[h.rightChild(index)] {
+				biggestChild = h.rightChild(index)
+			}
+			if h.arr[biggestChild] < h.arr[index] {
+				h.arr[biggestChild], h.arr[index] = h.arr[index], h.arr[biggestChild]
+				index = biggestChild
+			} else {
+				return
+			}
+		}
+		if h.arr[biggestChild] < h.arr[index] {
+			h.arr[biggestChild], h.arr[index] = h.arr[index], h.arr[biggestChild]
+		}
+		index = biggestChild
+
+	}
+}
+
+func (h *MinHeapPrac) Delete(num int) {
+	for i, v := range h.arr {
+		if v == num {
+			h.arr = append(h.arr[:i], h.arr[i:]...)
+		}
+	}
+}
+
+func (h *MinHeapPrac) Parent(index int) int {
+	return (index - 1) / 2
+}
+
+func (h *MinHeapPrac) leftChild(index int) int {
+	return (index * 2) + 1
+}
+
+func (h *MinHeapPrac) rightChild(index int) int {
+	return (index * 2) + 2
+}
--- Arrays/runArrays.go
+++ Arrays/runArrays.go
@@ -6,7 +6,16 @@ func RunArrays() {
 	fmt.Println("---Array Problems Have Begun---")
 	fmt.Println("Shuffle String:", ShuffleString("codeleet", []int{4,5,6,7,0,2,1,3}))
 	fmt.Println("Product of Array Except itself:", ProductExceptSelf([]int{1,2,3,4}))
-	fmt.Println("Validate Sudoku:", ValidSudoku([][]byte{{'5','3','.','.','7','.','.','.','.'},{'6','.','.','1','9','5','.','.','.'},{'.','9','8','.','.','.','.','6','.'},{'8','.','.','.','6','.','.','.','3'},{'4','.','.','8','.','3','.','.','1'},{'7','.','.','.','2','.','.','.','6'},{'.','6','.','.','.','.','2','8','.'},{'.','.','.','4','1','9','.','.','5'},{'.','.','.','.','8','.','.','7','9'}}))
+	fmt.Println("Validate Sudoku:", ValidSudoku([][]byte{
+		{'5','3','.','.','7','.','.','.','.'},
+		{'6','.','.','1','9','5','.','.','.'},
+		{'.','9','8','.','.','.','.','6','.'},
+		{'8','.','.','.','6','.','.','.','3'},
+		{'4','.','.','8','.','3','.','.','1'},
+		{'7','.','.','.','2','.','.','.','6'},
+		{'.','6','.','.','.','.','2','8','.'},
+		{'.','.','.','4','1','9','.','.','5'},
+		{'.','.','.','.','8','.','.','7','9'}}))
 	fmt.Println("Group Anagrams", GroupAnagrams([]string{"eat","tea","tan","ate","nat","bat"}))
 	fmt.Println("FindKthLargest", FindKthLargest([]int{3,2,1,5,6,4},2))
 	fmt.Println("Longest Consecutive Sequence", LongestConsecutive([]int{100,4,200,1,3,2}))
@@ -15,6 +24,7 @@ func RunArrays() {
 	fmt.Println("Top K Frequent",TopKFrequent([]int{1,1,1,2,2,3},2))
 	fmt.Println("Two Sum",TwoSum([]int{2,7,11,15},9))
 	fmt.Println("Is Anagram",IsAnagramStringTrick("hello","llohe"))
+	fmt.Println("Is Valid Palindrome",IsPalindrome("A man, a plan, a canal: Panama"))
 
 
 	//Practice Problems
@@ -22,12 +32,22 @@ func RunArrays() {
 	fmt.Println("Shuffle String Practice:", ShuffleString("codeleet", []int{4,5,6,7,0,2,1,3}))
 	fmt.Println("Group Anagrams Practice",GroupAnagramsPract([]string{"eat","tea","tan","ate","nat","bat"}))
 	fmt.Println("FindKthLargest Practice",FindKthLargestPrac([]int{3,2,1,5,6,4},2))
-	fmt.Println("Longest Consecutive Sequence Practice",LongestConsercutiveSequencePrac([]int{0,3,7,2,5,8,4,6,0,1}))
-	fmt.Println("Missing Number Practice", MissingNumberPrac([]int{3,0,1}))
+	fmt.Println("Longest Consecutive Sequence Practice",LongestConsercutiveSequencePrac([]int{200,100,1,3,2,4}))
+	fmt.Println("Missing Number Practice", MissingNumberPrac([]int{0,1,2,3}))
 	fmt.Println("Product Except Self Pracitce",ProductExceptSelfPrac([]int{1,2,3,4}))
 	fmt.Println("Top K Frequent Practice", TopKFrequentPrac([]int{1,1,1,2,2,3},2))
 	fmt.Println("Two Sum Practice")
 	fmt.Println("Is Anagram Practice",IsAnamgramPrac("hello","olelh"))
-	fmt.Println("Valid Sudoku Practice", ValidSudokuPrac([][]byte{{'5','3','.','.','7','.','.','.','.'},{'6','.','.','1','9','5','.','.','.'},{'.','9','8','.','.','.','.','6','.'},{'8','.','.','.','6','.','.','.','3'},{'4','.','.','8','.','3','.','.','1'},{'7','.','.','.','2','.','.','.','6'},{'.','6','.','.','.','.','2','8','.'},{'.','.','.','4','1','9','.','.','5'},{'.','.','.','.','8','.','.','7','9'}}))
+	fmt.Println("Valid Sudoku Practice", ValidSudokuPrac([][]byte{
+		{'5','3','.','.','7','.','.','.','.'},
+		{'6','.','.','1','9','5','.','.','.'},
+		{'.','9','8','.','.','.','.','6','.'},
+		{'8','.','.','.','6','.','.','.','3'},
+		{'4','.','.','8','.','3','.','.','1'},
+		{'7','.','.','.','2','.','.','.','6'},
+		{'.','6','.','.','.','.','2','8','.'},
+		{'.','.','.','4','1','9','.','.','5'},
+		{'.','.','.','.','8','.','.','7','9'}}))
 	fmt.Println("Shuffle String",ShuffleStringPrac("codeleet", []int{4,5,6,7,0,2,1,3}))
+	fmt.Println("Is Valid Palindrome",isValidPrac("A man, a plan, a canal: Panama"))
 }
