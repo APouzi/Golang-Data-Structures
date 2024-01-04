30c0aeff4fe1542df10c7ce72262801eae084d6b
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2024-01-04 04:21:46 +0100

    added some daily temp stuff -AP

--- Stack/runStack.go
+++ Stack/runStack.go
@@ -2,11 +2,13 @@ package stack
 
 import "fmt"
 
-func RunStack() {
+func RunStackProbs() {
 	fmt.Println("\nisValid started")
 	// s := "()" //Pass
 	s := "(())[]" //Pass
 	// s := "){" //Fail
 	// s:= "[(([))]]" //Fail
 	fmt.Println(isValid(s))
+
+	fmt.Println("Daily temperatures:", dailyTemperatures([]int{73,74,75,71,69,72,76,73}))
 }
