607052b6da0ed9b988bef9cf8039a03412a0dfaf
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2024-01-04 02:37:38 +0100

    fixed binary heap, remember the length is the issue. Such a silly move. Also simplified it to the greatest extent -AP

--- LinkedList/LLUtils.go
+++ LinkedList/LLUtils.go
@@ -11,46 +11,47 @@ func (h *MinHeapNode) Insert(head *LinkedNode) {
 
 func (h *MinHeapNode) Pop() *LinkedNode {
 	popped := h.arr[0]
-	h.arr[0] = h.arr[len(h.arr)-1]
-	h.arr = h.arr[:len(h.arr)-1]
+	var length int = len(h.arr) - 1
+	h.arr[0] = h.arr[length]
+	h.arr = h.arr[:length]
 	h.HeapifyDown(0)
 	return popped
 }
 
 func (h *MinHeapNode) HeapifyUp(num int) {
-	for h.Parent(num) >= 0 && h.arr[h.Parent(num)].Val > h.arr[num].Val {
+	for h.arr[h.Parent(num)].Val > h.arr[num].Val {
 		h.Swap(h.Parent(num), num)
 		num = h.Parent(num)
 	}
 }
 
-//	1
-//
-// 1 2
 func (h *MinHeapNode) HeapifyDown(num int) {
 	var smallestChild int
-	for h.LeftChild(num) < len(h.arr)-1 {
+	var length int = len(h.arr) - 1
+	for h.LeftChild(num) <= length {
 		smallestChild = h.LeftChild(num)
-		if h.RightChild(num) < len(h.arr)-1 && h.arr[smallestChild].Val > h.arr[h.RightChild(num)].Val {
-			smallestChild = h.RightChild(num)
-		}
-		if h.arr[smallestChild].Val < h.arr[num].Val {
-			h.Swap(num, smallestChild)
+		if h.RightChild(num) <= length {
+			if h.arr[h.RightChild(num)].Val < h.arr[smallestChild].Val {
+				smallestChild = h.RightChild(num)
+			}
+			if h.arr[smallestChild].Val < h.arr[num].Val {
+				h.Swap(smallestChild, num)
+			} else {
+				break
+			}
 		} else {
-			break
+			if h.arr[smallestChild].Val < h.arr[num].Val {
+				h.Swap(num, smallestChild)
+			}
 		}
 		num = smallestChild
 	}
-	// if h.RightChild(num) < len(h.arr) && h.arr[h.RightChild(num)].Val < h.arr[num].Val {
-	// 	h.Swap(h.RightChild(num), num)
-	// }
 }
 
 func (h *MinHeapNode) Swap(a, b int) {
 	h.arr[a], h.arr[b] = h.arr[b], h.arr[a]
 }
 
-// 1,2,3,4,5
 func (h *MinHeapNode) Parent(num int) int {
 	return (num - 1) / 2
 }
