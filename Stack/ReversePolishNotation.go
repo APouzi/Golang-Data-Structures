d3d8f024bdb0d71980900a716524d506b481a5bb
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2024-01-05 03:06:37 +0100

    added reverse polish notatation and run stack -AP

---
+++ Stack/ReversePolishNotation.go
@@ -0,0 +1,66 @@
+package stack
+
+import (
+	"strconv"
+)
+
+//You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+//Evaluate the expression. Return an integer that represents the value of the expression.
+
+// Note that:
+// The valid operators are '+', '-', '*', and '/'.
+// Each operand may be an integer or another expression.
+// The division between two integers always truncates toward zero.
+// There will not be any division by zero.
+// The input represents a valid arithmetic expression in a reverse polish notation.
+// The answer and all the intermediate calculations can be represented in a 32-bit integer.
+
+// Example 1:
+// Input: tokens = ["2","1","+","3","*"]
+// Output: 9
+// Explanation: ((2 + 1) * 3) = 9
+
+// Example 2:
+// Input: tokens = ["4","13","5","/","+"]
+// Output: 6
+// Explanation: (4 + (13 / 5)) = 6
+
+// Example 3:
+// Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+// Output: 22
+// Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+// = ((10 * (6 / (12 * -11))) + 17) + 5
+// = ((10 * (6 / -132)) + 17) + 5
+// = ((10 * 0) + 17) + 5
+// = (0 + 17) + 5
+// = 17 + 5
+// = 22
+
+
+func ReversePolishNotation(list []string) int {
+	var stack []int = make([]int, 0)
+	var ans int
+	for i := 0; i < len(list); i++ {
+		if list[i] == "+" {
+			ans = stack[len(stack)-2] + stack[len(stack)-1]
+			stack = stack[:len(stack)-2]
+			stack = append(stack, ans)
+		} else if list[i] == "*" {
+			ans = stack[len(stack)-2] * stack[len(stack)-1]
+			stack = stack[:len(stack)-2]
+			stack = append(stack, ans)
+		} else if list[i] == "-" {
+			ans = stack[len(stack)-2] - stack[len(stack)-1]
+			stack = stack[:len(stack)-2]
+			stack = append(stack, ans)
+		} else if list[i] == "/" {
+			ans = stack[len(stack)-2] / stack[len(stack)-1]
+			stack = stack[:len(stack)-2]
+			stack = append(stack, ans)
+		} else {
+			nums, _ := strconv.Atoi(list[i])
+			stack = append(stack, nums)
+		}
+	}
+	return stack[len(stack)-1]
+}
