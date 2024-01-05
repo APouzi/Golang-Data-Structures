a2a98edc82305812ae9896532d5588a6924378fa
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2024-01-05 03:07:55 +0100

    problem generator updated -AP

--- ProblemListGeneration/ProblemGenerator.go
+++ ProblemListGeneration/ProblemGenerator.go
@@ -13,13 +13,12 @@ var AllProblems []string = []string{
 	"valid anagram", "Sort a Linked List", "Quick Sort", "Merge Sort", "Binary Search", "Permutation of a String", 
 	"shuffle string", "Breadth First on BST", "BST Largest Sum Path", "Right View of Tree", "Longest Diameter of BST",
 	"Right View of Tree", "Sum Path BST", "Validate BST", "In Order, Post Order, Pre Order Traversal of a Tree",
-	"Check Sum BST", "Buy High Sell Low","Contains Duplicate", "Kth Smallest Element in a BST", "All Paths with Sum to 'k'",
-	"Merge K Sorted Lists", "Longest Substring without repeating Characters", "Implement a Trie", "Fruits in a Basket",
+	"Check Sum BST", "Buy High Sell Low","Contains Duplicate", "Kth Smallest Element in a BST", "All Paths with Sum to 'k'", "Longest Substring without repeating Characters", "Implement a Trie", "Fruits in a Basket",
 	"Count all substring that equal to 'k", "Implement a Trie", "Keys and Rooms (Topological Sorting)", 
 	"Sum of Nodes with Even-Valued Grandparent", "Course Schedule II", "Remove Nth Node From a List",
 	"SubArray Product less than K","Merge K Sorted Lists","Merge 2 Sorted Lists", "Intersection of two Linked Lists",
 	 "Maximum Subarray", "Delete Node from a BST", "Two Sum", "Path Sum II", "Invert a Binary Tree", "Meeting Rooms",
-	"Meeting Rooms II", "Remove Elements", "Skyline Problem", "Top K frequent Elements", "Group Anagrams", "Product of Array except itself", "kth most frequent element in array", "kth largest element in Array", "valid sudoku", "Maximum Sliding Window", "Minimum Window Substring", "Longest Substring Without Repeating Characters", "Minimum Interval From Query", "2 Sum II", "3 Sum" , "Container With Most Water"}
+	"Meeting Rooms II", "Remove Elements", "Skyline Problem", "Top K frequent Elements", "Group Anagrams", "Product of Array except itself", "kth most frequent element in array", "kth largest element in Array", "valid sudoku", "Maximum Sliding Window", "Minimum Window Substring", "Longest Substring Without Repeating Characters", "Minimum Interval From Query", "2 Sum II", "3 Sum" , "Container With Most Water", "Daily Temperatures","Reverse Polish Notation"}
 func GenerationProblemOne() {
 	fmt.Println("\nTotal Problems in Knowledge Bank is:",len(AllProblems))
 	fmt.Println("\n",AllProblems[rand.Intn(len(AllProblems))])
