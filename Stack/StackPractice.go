9a83a27fabbba26ef78b7e59c502d46585b387eb
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2024-01-06 02:24:39 +0100

    added stack practice file to practice stack problems -AP

---
+++ Stack/StackPractice.go
@@ -0,0 +1,56 @@
+package stack
+
+// Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+
+// Example 1:
+// Input: temperatures = [73,74,75,71,69,72,76,73]
+// Output: [1,1,4,2,1,1,0,0]
+
+// Example 2:
+// Input: temperatures = [30,40,50,60]
+// Output: [1,1,1,0]
+
+// Example 3:
+// Input: temperatures = [30,60,90]
+// Output: [1,1,0]
+
+func dailyTemperaturesPrac(temperatures []int) []int {
+	return []int{}
+}
+
+//You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+//Evaluate the expression. Return an integer that represents the value of the expression.
+
+// Note that:
+// The valid operators are '+', '-', '*', and '/'.
+// Each operand may be an integer or another expression.
+// The division between two integers always truncates toward zero.
+// There will not be any division by zero.
+// The input represents a valid arithmetic expression in a reverse polish notation.
+// The answer and all the intermediate calculations can be represented in a 32-bit integer.
+
+// Example 1:
+// Input: tokens = ["2","1","+","3","*"]
+// Output: 9
+// Explanation: ((2 + 1) * 3) = 9
+
+// Example 2:
+// Input: tokens = ["4","13","5","/","+"]
+// Output: 6
+// Explanation: (4 + (13 / 5)) = 6
+
+// Example 3:
+// Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+// Output: 22
+// Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+// = ((10 * (6 / (12 * -11))) + 17) + 5
+// = ((10 * (6 / -132)) + 17) + 5
+// = ((10 * 0) + 17) + 5
+// = (0 + 17) + 5
+// = 17 + 5
+// = 22
+
+
+func ReversePolishNotationPrac(list []string) int {
+	return -1
+}
