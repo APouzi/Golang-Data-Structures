08908ef8acd24b5b9bd2badd25d72c351ee74c8f
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2024-01-06 02:08:04 +0100

    pattern practice, pattern run and some other changes -AP

--- Patterns/MergeInterval/SkyLineProblem.go
+++ Patterns/MergeInterval/SkyLineProblem.go
@@ -1,6 +1,8 @@
 package patterns
 
-import "sort"
+import (
+	"sort"
+)
 
 //A city's skyline is the outer contour of the silhouette formed by all the buildings in that city when viewed from a distance. Given the locations and heights of all the buildings, return the skyline formed by these buildings collectively.
 
