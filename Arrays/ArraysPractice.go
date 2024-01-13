7c68638b730e1dc116fd67b53003ae6edecac292
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2024-01-13 01:07:20 +0100

    added max length between equal characters prac and a stack practice problem -AP

--- Arrays/ArraysPractice.go
+++ Arrays/ArraysPractice.go
@@ -203,3 +203,27 @@ func ShuffleStringPrac(s string, indices []int) string {
 func isValidPrac(s string) bool {
 	return false
 }
+
+// Given a string s, return the length of the longest substring between two equal characters, excluding the two characters. If there is no such substring return -1.
+// A substring is a contiguous sequence of characters within a string.
+
+// Example 1:
+// Input: s = "aa"
+// Output: 0
+// Explanation: The optimal substring here is an empty substring between the two 'a's.
+
+// Example 2:
+// Input: s = "abca"
+// Output: 2
+// Explanation: The optimal substring here is "bc".
+
+// Example 3:
+// Input: s = "cbzxy"
+// Output: -1
+// Explanation: There are no characters that appear twice in s.
+
+//Other TestCases: "mgntdygtxrvxjnwksqhxuxtrv" = 18, "ojdncpvyneq" = 4, "scayofdzca" = 6
+
+func maxLengthBetweenEqualCharactersPrac(s string) int {
+	return 0
+}
