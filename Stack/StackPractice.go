44441144a583bafca291ca32a53ccfce825adc53
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2024-01-19 03:35:15 +0100

    added practice for histogram -AP

--- Stack/StackPractice.go
+++ Stack/StackPractice.go
@@ -126,5 +126,18 @@ func generateParenthesisPrac(n int) []string {
 
 
 func carFleetPrac(target int, position []int, speed []int) int {
+
+
+
+// Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.
+
+// Example 1:
+// Input: heights = [2,1,5,6,2,3]
+// Output: 10
+// Explanation: The above is a histogram where width of each bar is 1.
+// The largest rectangle is shown in the red area, which has an area = 10 units.
+
+
+func largestRectangleAreaPrac(heights []int) int {
 	return -1
 }
