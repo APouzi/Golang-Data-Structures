ace080d4aeb3e2beb0e93cde149a30ced3f93163
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2024-01-22 00:39:03 +0100

    Binary Search for koko comments explanation -AP

--- Patterns/BinarySearch/KokoEatingBananas.go
+++ Patterns/BinarySearch/KokoEatingBananas.go
@@ -1,5 +1,7 @@
 package binarysearch
 
+import "math"
+
 // Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
 
 // Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
@@ -20,8 +22,34 @@ package binarysearch
 // Input: piles = [30,11,23,4,20], h = 6
 // Output: 23
 
-
 func MinEatingSpeed(piles []int, h int) int {
+	//Step one is to create an options list that we are going to do a binary search on. This options list will be the minimum amount koko can eat from the pile, 1. To the max amount. This list is how we can perform the binary search to find out the minimum amount of bananas we can eat per hour, without surpassing the hour.
+    var left, right = 1, maxPilesSize(piles)
+    var result int = right
+    var middle int
+    var hours float64 = 0
+    h64 := float64(h)
+    for left <= right{
+        middle = (right + left)/2 //The middle, otherwise known as "k", is the canidate we have to see if this we add up the time, it's less than the hours (h) given. 
+        hours = 0
+        for _, v := range piles{ //Calculate if the canidate eating rate will fit into the given hours before gaurds come back.
+            hours += math.Ceil(float64(v)/float64(middle))
+        }
+
+        if hours <= h64 { //If the hours are less than the hours we have before gaurds come back, this means we can lessen the possible canidates by half and those larger the middle. We also want to continually see if that's the minimum possible.
+            result = min(result, middle)
+            right = middle -1
+        }else if hours >= h64 { // If the hours given is too big, it means we can lessen by half, but starting from the left.
+            left = middle + 1
+        }
+    }
+
+    return result
+}
+
+
+func MinEatingSpeedV2(piles []int, h int) int {
+	
 	var left, right = 1, maxPilesSize(piles)
 	var result int = right
 	var middle int
