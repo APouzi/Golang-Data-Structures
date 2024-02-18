ba37021e094133411d1d6c2266e9d2f59fb47afa
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2024-02-18 01:02:22 -0800

    added description to problem for find dup in array -AP

--- Patterns/FastAndSlow/FindDuplicateElementInArray.go
+++ Patterns/FastAndSlow/FindDuplicateElementInArray.go
@@ -1,5 +1,17 @@
 package fastandslow
 
+// Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+// There is only one repeated number in nums, return this repeated number.
+// You must solve the problem without modifying the array nums and uses only constant extra space.
+
+// Example 1:
+// Input: nums = [1,3,4,2,2]
+// Output: 2
+
+// Example 2:
+// Input: nums = [3,1,3,4,2]
+// Output: 3
+
 func FindDuplicate(nums []int) int {
 	var slow, fast int = nums[0], nums[nums[0]]
 	// These array's are only representing the next index that it goes to, because these are cyclical arrays that basically point to the next index. So calling itself will allow us to infinitely call the array without ever getting out of bounds. Which is why we first must find the "entry point" of the cycle with the cycle detection method.
