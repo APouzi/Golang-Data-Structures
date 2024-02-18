7176b9cf38b8293fe5533ba50ad885767c87b337
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2024-02-18 00:30:24 -0800

    removed practice problems that were copies -AP

--- Patterns/PatternPractice.go
+++ Patterns/PatternPractice.go
@@ -89,18 +89,33 @@ func nonOverLappingIntervalsPrac(intervals [][]int) int {
 func MinIntervalPrac(intervals [][]int, queries []int)[]int{
 	return []int{}
 }
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
+// Example 1:
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+// Example 2:
+// Input: prices = [7,6,4,3,1]
+// Output: 0
+// Explanation: In this case, no transactions are done and the max profit = 0.
 func BuyHighSellLowPrac(prices []int) int{
-	return -1
+	var min int = prices[0]
+	var profit int = 0
+	for _, v := range prices{
+		if v < min{
+			min = v
+		}
+		profit = max(profit, v - min)	
+	}
+	return profit
 }
 
-func ValidPalindromePrac(word string) bool{
-	return false
-}
 
-func FindDuplicateInArrayPrac([]int)int{
-	return -1
-}
 
 //A city's skyline is the outer contour of the silhouette formed by all the buildings in that city when viewed from a distance. Given the locations and heights of all the buildings, return the skyline formed by these buildings collectively.
 
