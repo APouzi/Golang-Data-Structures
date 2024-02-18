fb7ac6a704b3066c133493864368bf05e44b395d
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2024-02-18 00:28:57 -0800

    moved find duplicate problem over -AP

--- Arrays/ArraysPractice.go
+++ Arrays/ArraysPractice.go
@@ -227,3 +227,19 @@ func isValidPrac(s string) bool {
 func maxLengthBetweenEqualCharactersPrac(s string) int {
 	return 0
 }
+
+
+// Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+// There is only one repeated number in nums, return this repeated number.
+// You must solve the problem without modifying the array nums and uses only constant extra space.
+
+// Example 1:
+// Input: nums = [1,3,4,2,2]
+// Output: 2
+
+// Example 2:
+// Input: nums = [3,1,3,4,2]
+// Output: 3
+func FindDuplicateInArrayPrac([]int)int{
+	return -1
+}
