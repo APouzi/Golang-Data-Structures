9615020fb8c07e3a95ff33d62542e56b49cb5b2a
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2024-02-18 17:31:52 -0800

    added prac for majority elem -AP

--- Arrays/ArraysPractice.go
+++ Arrays/ArraysPractice.go
@@ -243,3 +243,21 @@ func maxLengthBetweenEqualCharactersPrac(s string) int {
 func FindDuplicateInArrayPrac(nums []int)int{
 	return -1
 }
+
+
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+// Example 1:
+// Input: nums = [3,2,3]
+// Output: 3
+
+// Example 2:
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+
+
+func MajorityElementPrac(nums []int) int {
+	return -1
+}
