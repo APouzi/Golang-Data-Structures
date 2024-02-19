df260ac807d9a1f4bf4861369f91d76608e7a65c
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2024-02-18 17:33:08 -0800

    master list incremented -AP

--- ProblemListGeneration/NeedToMaster.go
+++ ProblemListGeneration/NeedToMaster.go
@@ -6,7 +6,7 @@ import (
 )
 
 var MasterThese []string = []string{
-	"Trapping Rain Water", "Container With Most Water", "Two Sum II", "Longest Repeating Characters With Replacement", "Max Sliding Window", "Skyline Problem", "Minimum Interval from Query", "Meeting Room II", "Maximum Population", "Happy Number", "Find Duplicate Element In Array", "Return Linked List Cycle start (Floyds)", "Reverse K Groups", "Reverse Linked List II", "Remove Nth node", "Missing Number", "Is Valid Palindrome", "Valid Sudoku", "Product of Array except itself", "Group Anagrams", "Valid Anagram", "Rotate Image", "rotate matrix",
+	"Trapping Rain Water", "Container With Most Water", "Two Sum II", "Longest Repeating Characters With Replacement", "Max Sliding Window", "Skyline Problem", "Minimum Interval from Query", "Meeting Room II", "Maximum Population", "Happy Number", "Find Duplicate Element In Array", "Return Linked List Cycle start (Floyds)", "Reverse K Groups", "Reverse Linked List II", "Remove Nth node", "Missing Number", "Is Valid Palindrome", "Valid Sudoku", "Product of Array except itself", "Group Anagrams", "Valid Anagram", "Rotate Image", "rotate matrix", "Largest Substring between two equal characters",
 }
 
 func MasterGenerationProblemOne() {
