92b677b1678b60d74654e280e5d499f53a199f3d
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2024-02-25 20:27:18 -0800

    fixed the runs and formatting -AP

--- Patterns/PatternRun.go
+++ Patterns/PatternRun.go
@@ -45,10 +45,9 @@ func RunPattern() {
 
 	//---Insert Intervals---
 	intervalsIns := [][]int{{1,2},{3,5},{6,7},{8,10},{12,16}}
-	fmt.Println("\nInsert Intervals:",InsertIntervals(intervalsIns,[]int{4,8}))
-
-	intervalsInsPrac := [][]int{{1,2},{3,5},{6,7},{8,10},{12,16}}
-	fmt.Println("Insert Intervals Prac:",InsertIntervalsPrac(intervalsInsPrac,[]int{4,8}))
+	newInterval := []int{4,8}
+	fmt.Println("\nInsert Intervals:",InsertIntervals(intervalsIns,newInterval))
+	fmt.Println("Insert Intervals Prac:",InsertIntervalsPrac(intervalsIns,newInterval))
 
 	//---Non OverLapping Intervals---
 	// intervalsRem := [][]int{{1,2},{2,3},{3,4},{1,3}}
@@ -70,11 +69,13 @@ func RunPattern() {
 	meetings := [][]int{{0,30},{5,10},{15,20}}
 	// meetings := [][]int{{7,10},{2,4}}
 	fmt.Println("\nMeeting Rooms:", MeetingRooms(meetings))
+	fmt.Println("Meeting Rooms Prac:", MeetingRoomsPrac(meetings))
 
 	// meetingsII := [][]int{{0, 30},{5, 10},{15, 20}}
 	meetingsII := [][]int{{7,10},{2,4}}
 	fmt.Println("\nMeeting Rooms II:",MeetingRoomsII(meetingsII))
-	fmt.Println("Meeting Roomms II",MeetingRoomsIIHeap(meetingsII))
+	fmt.Println("Meeting Roomms II Heap",MeetingRoomsIIHeap(meetingsII))
+	fmt.Println("Meeting Roomms II Prac:",MeetingRoomsIIPrac(meetingsII))
 
 	//---Maximum Year---
 	// Years := [][]int{{1950,1961},{19,60,1971},{1970,1981}}
@@ -132,29 +133,29 @@ func RunPattern() {
 	//---Trapping rain water---
 	var trappingRainWaterInput = []int{0,1,0,2,1,0,1,3,2,1,2,1}
 	fmt.Println("\nTrapping Rain Water",twopointers.TrappingRainWater(trappingRainWaterInput))
-	fmt.Println("\nTrapping Rain Water Practice",TrappingRainWater(trappingRainWaterInput))
+	fmt.Println("Trapping Rain Water Practice",TrappingRainWater(trappingRainWaterInput))
 
 	//---Binary Search---
 	var binarySearchInput []int = []int{-1,0,3,5,9,12}
 	fmt.Println("\nBinary Search:",binarysearch.BinarySearch(binarySearchInput, 9))
-	fmt.Println("\nBinary Search Practice:",BinarySearch(binarySearchInput, 9))
+	fmt.Println("Binary Search Practice:",BinarySearch(binarySearchInput, 9))
 
 	//---Search A 2D Array---
 	var TwoDArray [][]int = [][]int{{1,3,5,7},{10,11,16,20},{23,30,34,60}}
 	//var TwoDArrayFalse [][]int = [][]int{{1,3,5,7},{10,11,16,20},{23,30,34,60}} // target = 13
-	fmt.Println("Search a 2D Array", binarysearch.SearchMatrixV2(TwoDArray,16))
+	fmt.Println("\nSearch a 2D Array", binarysearch.SearchMatrixV2(TwoDArray,16))
 	fmt.Println("Search a 2D Array Prac", SearchMatrix(TwoDArray,16))
 
 	//---Koko Eating a Banana---
 	var pilesBanana []int = []int{30,11,23,4,20}
 	var hour int = 5
-	fmt.Println("Koko Eating a Banana:", binarysearch.MinEatingSpeed(pilesBanana, hour))
+	fmt.Println("\nKoko Eating a Banana:", binarysearch.MinEatingSpeed(pilesBanana, hour))
 	fmt.Println("Koko Eating a Banana Prac:", MinEatingSpeed(pilesBanana, hour))
 
 
 	//---Find Minimum In Rotated Array---
 	var rotateArr []int = []int{4,5,6,7,0,1,2}
-	fmt.Println("Find Minimum In Rotated Array:",binarysearch.FindMinInRotatedArray(rotateArr))
+	fmt.Println("\nFind Minimum In Rotated Array:",binarysearch.FindMinInRotatedArray(rotateArr))
 	fmt.Println("Find Minimum In Rotated Array Prac:",findMinInRotatedArrayPrac(rotateArr))
 
 
