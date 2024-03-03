c5a844c2080ebedd12e602ffe495f4a057a63c68
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2024-03-03 03:29:33 -0800

    variable caching -AP

--- Stack/ValidParenthesis.go
+++ Stack/ValidParenthesis.go
@@ -4,6 +4,7 @@ func isValid(s string) bool {
 	var open map[rune]rune = map[rune]rune{'(': ')', '{': '}', '[': ']'}
 
 	var stack []rune = []rune{}
+	var popped rune
 	for _, v := range s {
 		if _, ok := open[v]; ok {
 			stack = append(stack, open[v])
@@ -11,7 +12,7 @@ func isValid(s string) bool {
 			if len(stack) == 0 {
 				return false
 			}
-			var popped rune = stack[len(stack)-1]
+			popped = stack[len(stack)-1]
 			stack = stack[:len(stack)-1]
 			if v != popped {
 				return false
