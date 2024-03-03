57e49b002ba9f1beda295293e108c1b1835e9e68
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2024-03-03 03:40:30 -0800

    sliding window prac and run -AP

--- Patterns/PatternRun.go
+++ Patterns/PatternRun.go
@@ -3,7 +3,6 @@ package patterns
 import (
 	"fmt"
 
-	binarysearch "github.com/APouzi/go-algos/Patterns/BinarySearch"
 	fastandslow "github.com/APouzi/go-algos/Patterns/FastAndSlow"
 	slidingwindow "github.com/APouzi/go-algos/Patterns/SlidingWindow"
 	patterns "github.com/APouzi/go-algos/Patterns/mergeintervals"
@@ -85,28 +84,7 @@ func RunPattern() {
 	fmt.Println("\nInterval List Intersection:", patterns.IntervalIntersection(firstList,secondList))
 	fmt.Println("Interval List Intersection Prac:", IntervalIntersection(firstList,secondList))
 
-	//---Binary Search---
-	var binarySearchInput []int = []int{-1,0,3,5,9,12}
-	fmt.Println("\nBinary Search:",binarysearch.BinarySearch(binarySearchInput, 9))
-	fmt.Println("Binary Search Practice:",BinarySearch(binarySearchInput, 9))
 
-	//---Search A 2D Array---
-	var TwoDArray [][]int = [][]int{{1,3,5,7},{10,11,16,20},{23,30,34,60}}
-	//var TwoDArrayFalse [][]int = [][]int{{1,3,5,7},{10,11,16,20},{23,30,34,60}} // target = 13
-	fmt.Println("\nSearch a 2D Array", binarysearch.SearchMatrixV2(TwoDArray,16))
-	fmt.Println("Search a 2D Array Prac", SearchMatrix(TwoDArray,16))
-
-	//---Koko Eating a Banana---
-	var pilesBanana []int = []int{30,11,23,4,20}
-	var hour int = 5
-	fmt.Println("\nKoko Eating a Banana:", binarysearch.MinEatingSpeed(pilesBanana, hour))
-	fmt.Println("Koko Eating a Banana Prac:", MinEatingSpeed(pilesBanana, hour))
-
-
-	//---Find Minimum In Rotated Array---
-	var rotateArr []int = []int{4,5,6,7,0,1,2}
-	fmt.Println("\nFind Minimum In Rotated Array:",binarysearch.FindMinInRotatedArray(rotateArr))
-	fmt.Println("Find Minimum In Rotated Array Prac:",findMinInRotatedArrayPrac(rotateArr))
 
 
 }
